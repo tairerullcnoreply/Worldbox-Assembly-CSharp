9bf6f2c6d2ed6f90fda9c6e37e77a0677cac10c9
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:05:54 -0500

    Add BuildingWaypointGoldenEgg.cs

---
+++ BuildingWaypointGoldenEgg.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BuildingWaypointGoldenEgg
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class BuildingWaypointGoldenEgg : BuildingWaypoint
+{
+  protected override string effect_id => "fx_waypoint_golden_egg_launch_bottom";
+
+  protected override string trait_id => "desire_golden_egg";
+}
