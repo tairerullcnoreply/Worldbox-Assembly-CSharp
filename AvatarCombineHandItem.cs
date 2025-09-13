cce71c1757e0e057964a60cc432bb2bfec53ecdb
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:02:18 -0500

    Add AvatarCombineHandItem.cs

---
+++ AvatarCombineHandItem.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AvatarCombineHandItem
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class AvatarCombineHandItem
+{
+  public readonly IHandRenderer hand_renderer;
+
+  public AvatarCombineHandItem(IHandRenderer pHandRenderer) => this.hand_renderer = pHandRenderer;
+}
