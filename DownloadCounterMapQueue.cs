f9f0d4e69d59844fcf172f608d688c5f119ddf77
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:09:42 -0500

    Add DownloadCounterMapQueue.cs

---
+++ DownloadCounterMapQueue.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DownloadCounterMapQueue
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+[Serializable]
+public class DownloadCounterMapQueue : QueueItem
+{
+  public string username;
+  public string userId;
+  public string reason;
+  public string error;
+  public string status;
+  public string mapId;
+}
