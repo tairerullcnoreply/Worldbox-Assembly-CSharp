c49eb03d3f7dfd6ee6a9eee76d0beb9f1180f300
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:22:25 -0500

    Add SpritePool.cs

---
+++ SpritePool.cs
@@ -0,0 +1,14 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SpritePool
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+using UnityEngine;
+
+#nullable disable
+public class SpritePool
+{
+  internal List<Sprite> list = new List<Sprite>();
+}
