d945eea25875ee0c8b48694a70a2b74079ef711b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:12 -0500

    Add BehaviourActionCity.cs

---
+++ BehaviourActionCity.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehaviourActionCity
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class BehaviourActionCity : BehaviourActionBase<City>
+{
+  public override bool errorsFound(City pCity)
+  {
+    return !pCity.hasZones() || pCity.getPopulationPeople() == 0 || base.errorsFound(pCity);
+  }
+
+  protected override void setupErrorChecks()
+  {
+    base.setupErrorChecks();
+    this.uses_kingdoms = true;
+    this.uses_cities = true;
+  }
+}
