c476a4dc86673cbdab479603860dc8d9f7300351
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:31:05 -0500

    Add ai/behaviours/BehGoOrSwimToTileTarget.cs

---
+++ ai/behaviours/BehGoOrSwimToTileTarget.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehGoOrSwimToTileTarget
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehGoOrSwimToTileTarget : BehGoToTileTarget
+{
+  protected override void setupErrorChecks()
+  {
+    base.setupErrorChecks();
+    this.null_check_tile_target = true;
+    this.walk_on_water = true;
+  }
+}
