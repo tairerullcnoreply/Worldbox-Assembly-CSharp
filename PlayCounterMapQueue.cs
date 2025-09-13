abedd7060fbeebf95e1b1cee9c2cccb746c1898f
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:18:42 -0500

    Add PlayCounterMapQueue.cs

---
+++ PlayCounterMapQueue.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: PlayCounterMapQueue
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+[Serializable]
+public class PlayCounterMapQueue : QueueItem
+{
+  public string username;
+  public string userId;
+  public string reason;
+  public string error;
+  public string status;
+  public string mapId;
+}
