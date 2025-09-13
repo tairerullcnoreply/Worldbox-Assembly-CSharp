929f5d204ca9fec55fb1aa1de99776b570c28220
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:01:15 -0500

    Add AiSystemTester.cs

---
+++ AiSystemTester.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AiSystemTester
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class AiSystemTester : 
+  AiSystem<AutoTesterBot, JobTesterAsset, BehaviourTaskTester, BehaviourActionTester, BehaviourTesterCondition>
+{
+  public AiSystemTester(AutoTesterBot pObject)
+    : base(pObject)
+  {
+    this.jobs_library = (AssetLibrary<JobTesterAsset>) AssetManager.tester_jobs;
+    this.task_library = (AssetLibrary<BehaviourTaskTester>) AssetManager.tester_tasks;
+  }
+}
