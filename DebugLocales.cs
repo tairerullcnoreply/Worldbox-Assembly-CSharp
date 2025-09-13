b2badab62b236af3c1b8559e225f7f30df5a0185
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:09:02 -0500

    Add DebugLocales.cs

---
+++ DebugLocales.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DebugLocales
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class DebugLocales
+{
+  private const string TODO_TEXT = "TODO_TEXT";
+  private const string NO_TEXT = "???";
+
+  public static void init()
+  {
+    LocalizedTextManager.add("tab_technologies_description", "TODO_TEXT");
+  }
+}
