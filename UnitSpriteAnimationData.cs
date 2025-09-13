5184fd57acb877c100aa89627f593fb3ce2d89be
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:26:20 -0500

    Add UnitSpriteAnimationData.cs

---
+++ UnitSpriteAnimationData.cs
@@ -0,0 +1,27 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: UnitSpriteAnimationData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+using UnityEngine;
+
+#nullable disable
+[Serializable]
+public class UnitSpriteAnimationData
+{
+  public string name;
+  public Vector3 head;
+  public Vector3 item;
+  public Vector3 backpack;
+  public bool showHead;
+  public bool showItem;
+
+  public UnitSpriteAnimationData()
+  {
+    this.head = new Vector3();
+    this.head = new Vector3();
+    this.backpack = new Vector3();
+  }
+}
