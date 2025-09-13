006497efa4360ca94007ee6c6218f3d56b3ac334
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:09:55 -0500

    Add DropType.cs

---
+++ DropType.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DropType
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum DropType
+{
+  DropSeed,
+  DropBomb,
+  DropBuilding,
+  DropMineral,
+  DropCreep,
+  DropTraitRain,
+  DropEquipmentRain,
+  DropMagic,
+  DropGeneric,
+  DropTile,
+  DropStatus,
+  DropTrait,
+}
