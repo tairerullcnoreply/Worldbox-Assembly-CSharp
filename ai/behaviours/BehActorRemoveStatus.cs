7934c0da1a200eba3afb836ca4d64daad4852e44
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:28:43 -0500

    Add ai/behaviours/BehActorRemoveStatus.cs

---
+++ ai/behaviours/BehActorRemoveStatus.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehActorRemoveStatus
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehActorRemoveStatus : BehaviourActionActor
+{
+  private string status;
+
+  public BehActorRemoveStatus(string pStatus) => this.status = pStatus;
+
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.finishStatusEffect(this.status);
+    return BehResult.Continue;
+  }
+}
