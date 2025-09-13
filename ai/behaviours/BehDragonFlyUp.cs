a3b2c5216dc8ff8480fa7002283c6f29d743e0e9
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:12 -0500

    Add ai/behaviours/BehDragonFlyUp.cs

---
+++ ai/behaviours/BehDragonFlyUp.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehDragonFlyUp
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehDragonFlyUp : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.data.set("landAttacks", 0);
+    return BehResult.Continue;
+  }
+}
