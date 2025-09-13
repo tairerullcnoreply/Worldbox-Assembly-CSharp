f09a11c39728cf2dc7d43308ea452f95a3080b78
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:25:19 -0500

    Add TooltipOpinionInfo.cs

---
+++ TooltipOpinionInfo.cs
@@ -0,0 +1,12 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TooltipOpinionInfo
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public readonly struct TooltipOpinionInfo(string pTranslationKey, int pValue)
+{
+  public readonly string translation_key = pTranslationKey;
+  public readonly int value = pValue;
+}
