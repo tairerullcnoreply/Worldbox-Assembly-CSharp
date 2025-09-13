b7ce395b2f8ffb8c50c9b03314f0f37415fa5323
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:00:15 -0500

    Add ActorAnimation.cs

---
+++ ActorAnimation.cs
@@ -0,0 +1,14 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ActorAnimation
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class ActorAnimation
+{
+  public int id;
+  internal Sprite[] frames;
+}
