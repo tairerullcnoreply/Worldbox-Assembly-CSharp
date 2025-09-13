3c4f04345ec470165bcacf752b1d2c4f9558b041
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:09:12 -0500

    Add DebugToolTextElement.cs

---
+++ DebugToolTextElement.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DebugToolTextElement
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#nullable disable
+public class DebugToolTextElement : MonoBehaviour
+{
+  public Text text_left;
+  public Text text_right;
+  public Image text_bar;
+}
