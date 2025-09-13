f6934bee09fd7bc0edaf59cc75158dad0f43fd6e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:14 -0500

    Add ai/behaviours/BehDragonLandAttack.cs

---
+++ ai/behaviours/BehDragonLandAttack.cs
@@ -0,0 +1,36 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehDragonLandAttack
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehDragonLandAttack : BehDragon
+{
+  public override BehResult execute(Actor pActor)
+  {
+    SpriteAnimation spriteAnimation = this.dragon.spriteAnimation;
+    if (spriteAnimation.currentFrameIndex == 4)
+      pActor.data.set("shouldAttack", true);
+    if (spriteAnimation.currentFrameIndex == 5)
+    {
+      bool pResult1;
+      pActor.data.get("shouldAttack", out pResult1);
+      if (pResult1)
+      {
+        pActor.data.removeBool("shouldAttack");
+        foreach (WorldTile landAttackTile in this.dragon.landAttackTiles(pActor.current_tile))
+        {
+          if (landAttackTile != null && (landAttackTile.hasUnits() || !Randy.randomBool()))
+            this.dragon.attackTile(landAttackTile);
+        }
+        int pResult2;
+        pActor.data.get("landAttacks", out pResult2);
+        pActor.data.set("landAttacks", ++pResult2);
+      }
+    }
+    return spriteAnimation.currentFrameIndex < spriteAnimation.frames.Length - 1 ? BehResult.RepeatStep : BehResult.Continue;
+  }
+}
