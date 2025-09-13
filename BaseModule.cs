c232ace162426ba6f5a0df9089a5164eaa43c581
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:02:45 -0500

    Add BaseModule.cs

---
+++ BaseModule.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BaseModule
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class BaseModule : BaseMapObject
+{
+  internal HashSet<WorldTile> hashset;
+  protected float timer;
+
+  internal virtual void clear()
+  {
+  }
+}
