32bd5c89de3f97a93fa035c681f836755a19cddc
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:15:01 -0500

    Add List.cs

---
+++ List.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: List
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public static class List
+{
+  public static List<T> Of<T>(params T[] pArgs) => new List<T>((IEnumerable<T>) pArgs);
+}
