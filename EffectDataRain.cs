23937a15f338728738e2a105c7ae2b56378c38fc
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:10:07 -0500

    Add EffectDataRain.cs

---
+++ EffectDataRain.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EffectDataRain
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Runtime.InteropServices;
+
+#nullable disable
+[StructLayout(LayoutKind.Sequential, Size = 1)]
+public struct EffectDataRain
+{
+}
