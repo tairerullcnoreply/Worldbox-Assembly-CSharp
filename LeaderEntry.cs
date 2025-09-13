9d48a2035f36125cc9d220f46e6750972a4944cf
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:14:52 -0500

    Add LeaderEntry.cs

---
+++ LeaderEntry.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: LeaderEntry
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class LeaderEntry
+{
+  public long id;
+  public int color_id;
+  public string name;
+  public double timestamp_ago;
+  public double timestamp_end;
+}
