b58dcf29ed68f1734ffecab66a284a4d47364335
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:06:41 -0500

    Add CityBehCheckLoyalty.cs

---
+++ CityBehCheckLoyalty.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CityBehCheckLoyalty
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class CityBehCheckLoyalty : BehaviourActionCity
+{
+  public override BehResult execute(City pCity)
+  {
+    CityBehCheckLoyalty.check(pCity);
+    return BehResult.Continue;
+  }
+
+  public static void check(City pCity) => pCity.getLoyalty(true);
+}
