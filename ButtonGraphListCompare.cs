191f72b87992fdfd51e07426f89d956ea43d510d
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:06:04 -0500

    Add ButtonGraphListCompare.cs

---
+++ ButtonGraphListCompare.cs
@@ -0,0 +1,37 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ButtonGraphListCompare
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class ButtonGraphListCompare : MonoBehaviour
+{
+  public void compareListItems()
+  {
+    ScrollWindow currentWindow = ScrollWindow.getCurrentWindow();
+    IComponentList componentInChildren = ((Component) currentWindow).GetComponentInChildren<IComponentList>(true);
+    if (componentInChildren == null)
+    {
+      Debug.LogError((object) ("IComponentList missing in " + ((Object) ((Component) currentWindow).gameObject).name), (Object) ((Component) currentWindow).gameObject);
+    }
+    else
+    {
+      using (ListPool<NanoObject> elements = componentInChildren.getElements())
+      {
+        if (elements.Count > 0)
+        {
+          Config.selected_objects_graph.Clear();
+          for (int index = 0; index < elements.Count && index < 3; ++index)
+          {
+            NanoObject pObject = elements[index];
+            Config.selected_objects_graph.Add(pObject);
+          }
+        }
+        ScrollWindow.showWindow("chart_comparer");
+      }
+    }
+  }
+}
