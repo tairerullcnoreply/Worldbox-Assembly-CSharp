99b1a4468051c4c7f2816734e2bef7218638017b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:11:10 -0500

    Add GameObjectZSorter.cs

---
+++ GameObjectZSorter.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GameObjectZSorter
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class GameObjectZSorter : MonoBehaviour
+{
+  public GameObject zParent;
+  public float z_order_offset;
+
+  private void Start()
+  {
+  }
+}
