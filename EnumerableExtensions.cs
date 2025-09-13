b9b01fdc20e1917724927a7dbcf3ad6c73f42236
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:10:20 -0500

    Add EnumerableExtensions.cs

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EnumerableExtensions
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public static class EnumerableExtensions
+{
+  public static T GetRandom<T>(this IEnumerable<T> pEnumerable)
+  {
+    switch (pEnumerable)
+    {
+      case List<T> list1:
+        return list1.GetRandom<T>();
+      case ListPool<T> list2:
+        return list2.GetRandom<T>();
+      case T[] pArray:
+        return pArray.GetRandom<T>();
+      case HashSet<T> pHashSet:
+        return pHashSet.GetRandom<T>();
+      default:
+        using (ListPool<T> list = new ListPool<T>(pEnumerable))
+          return list.GetRandom<T>();
+    }
+  }
+}
