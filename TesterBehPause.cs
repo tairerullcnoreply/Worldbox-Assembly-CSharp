ca41a445652646f9d0a7fbdf696611b5c19f14c0
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:57 -0500

    Add TesterBehPause.cs

---
+++ TesterBehPause.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehPause
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class TesterBehPause : BehaviourActionTester
+{
+  private bool value;
+
+  public TesterBehPause(bool pValue) => this.value = pValue;
+
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    Config.paused = this.value;
+    return BehResult.Continue;
+  }
+}
