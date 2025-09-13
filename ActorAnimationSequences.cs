245c5293fcada4cdb9606c2bd91da343d1d08f77
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:00:16 -0500

    Add ActorAnimationSequences.cs

---
+++ ActorAnimationSequences.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ActorAnimationSequences
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class ActorAnimationSequences
+{
+  public static readonly string[] walk_0 = Toolbox.a<string>(nameof (walk_0));
+  public static readonly string[] walk_0_1 = Toolbox.a<string>(nameof (walk_0), "walk_1");
+  public static readonly string[] walk_0_2 = Toolbox.a<string>(nameof (walk_0), "walk_1", "walk_2");
+  public static readonly string[] walk_0_3 = Toolbox.a<string>(nameof (walk_0), "walk_1", "walk_2", "walk_3");
+  public static readonly string[] walk_0_4 = Toolbox.a<string>(nameof (walk_0), "walk_1", "walk_2", "walk_3", "walk_4");
+  public static readonly string[] walk_0_5 = Toolbox.a<string>(nameof (walk_0), "walk_1", "walk_2", "walk_3", "walk_4", "walk_5");
+  public static readonly string[] swim_0 = Toolbox.a<string>(nameof (swim_0));
+  public static readonly string[] swim_0_1 = Toolbox.a<string>(nameof (swim_0), "swim_1");
+  public static readonly string[] swim_0_2 = Toolbox.a<string>(nameof (swim_0), "swim_1", "swim_2");
+  public static readonly string[] swim_0_3 = Toolbox.a<string>(nameof (swim_0), "swim_1", "swim_2", "swim_3");
+  public static readonly string[] swim_0_5 = Toolbox.a<string>(nameof (swim_0), "swim_1", "swim_2", "swim_3", "swim_4", "swim_5");
+  public static readonly string[] idle_0_3 = Toolbox.a<string>("idle_0", "idle_1", "idle_2", "idle_3");
+}
