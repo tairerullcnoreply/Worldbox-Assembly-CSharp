245bd9d5b2ba713f934d1bd40b8d519123099988
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:15:38 -0500

    Add LoyaltyLibrary.cs

---
+++ LoyaltyLibrary.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: LoyaltyLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class LoyaltyLibrary : AssetLibrary<LoyaltyAsset>
+{
+  public override void init()
+  {
+    // ISSUE: unable to decompile the method.
+  }
+
+  public override void editorDiagnosticLocales()
+  {
+    foreach (LoyaltyAsset pAsset in this.list)
+    {
+      foreach (string localeId in pAsset.getLocaleIDs())
+        this.checkLocale((Asset) pAsset, localeId);
+    }
+    base.editorDiagnosticLocales();
+  }
+}
