240b4c53fb59303578e70341785254db21c8b78e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:08:22 -0500

    Add CultureColorsLibrary.cs

---
+++ CultureColorsLibrary.cs
@@ -0,0 +1,30 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CultureColorsLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+[Serializable]
+public class CultureColorsLibrary : ColorLibrary
+{
+  public CultureColorsLibrary() => this.file_path = "colors/colors_general";
+
+  public override void init()
+  {
+    base.init();
+    this.useSameColorsFrom((ColorLibrary) AssetManager.kingdom_colors_library);
+  }
+
+  public override bool isColorUsedInWorld(ColorAsset pAsset)
+  {
+    foreach (Culture culture in (CoreSystemManager<Culture, CultureData>) World.world.cultures)
+    {
+      if (this.checkColor(pAsset, culture.data.color_id))
+        return true;
+    }
+    return base.isColorUsedInWorld(pAsset);
+  }
+}
