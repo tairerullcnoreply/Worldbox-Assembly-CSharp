640356c6cc3b49a7c99dc3a83abf54903c550e2e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:14:23 -0500

    Add KingdomTraitsContainer.cs

---
+++ KingdomTraitsContainer.cs
@@ -0,0 +1,10 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: KingdomTraitsContainer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class KingdomTraitsContainer : TraitsContainer<KingdomTrait, KingdomTraitButton>
+{
+}
