a5978324d6b57e18254bf8ab501f038d3e5f0435
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:22:27 -0500

    Add SpriteSortingHelper.cs

---
+++ SpriteSortingHelper.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SpriteSortingHelper
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class SpriteSortingHelper
+{
+  public const float Z_BUILDING = -0.2f;
+  public const float OFFSET_BUILDING_LIGHT = -0.19f;
+  public const float OFFSET_FX_BOULDER = 4.5f;
+  public const float OFFSET_FX_STATUS_EFFECT = 0.01f;
+  public const float OFFSET_ITEMS = -0.01f;
+  public const float OFFSET_META_LINE = -0.02f;
+  public const float OFFSET_HAPPINESS = 18f;
+  public const float OFFSET_TASK_ICON = 17.5f;
+  public const float OFFSET_UNIT_BANNER = -0.01f;
+}
