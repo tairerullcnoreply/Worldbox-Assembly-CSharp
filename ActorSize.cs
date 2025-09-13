26fcea7cb7d00fd4e6dd059da814e780d7125c20
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:00:57 -0500

    Add ActorSize.cs

---
+++ ActorSize.cs
@@ -0,0 +1,28 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ActorSize
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum ActorSize
+{
+  S0_Bug,
+  S1_Egg,
+  S2_Crab,
+  S3_Rat,
+  S4_Piranha,
+  S5_Snake,
+  S6_Chicken,
+  S7_Cat,
+  S8_Fox,
+  S9_Monkey,
+  S10_Dog,
+  S11_Sheep,
+  S12_Wolf,
+  S13_Human,
+  S14_Cow,
+  S15_Bear,
+  S16_Buffalo,
+  S17_Dragon,
+}
