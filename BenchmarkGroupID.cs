edfdb52f1e08765bc2697bf046726a7be82235b1
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:04:34 -0500

    Add BenchmarkGroupID.cs

---
+++ BenchmarkGroupID.cs
@@ -0,0 +1,67 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BenchmarkGroupID
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class BenchmarkGroupID
+{
+  public const string main = "main";
+  public const string quantum_sprites = "quantum_sprites";
+  public const string world_beh = "world_beh";
+  public const string zone_camera = "zone_camera";
+  public const string zone_camera_total = "zone_camera_total";
+  public const string world_cache_manager = "world_cache_manager";
+  public const string sim_zones = "sim_zones";
+  public const string chunks = "chunks";
+  public const string chunks_total = "chunks_total";
+  public const string sprites_test = "sprites_test";
+  public const string sprites_test_total = "sprites_test_total";
+  public const string field_acess_test = "field_acess_test";
+  public const string field_acess_total = "field_acess_total";
+  public const string borders_renderer = "borders_renderer";
+  public const string borders_renderer_total = "borders_renderer_total";
+  public const string fluid_zones_data = "fluid_zones_data";
+  public const string fluid_zones_data_total = "fluid_zones_data_total";
+  public const string game_total = "game_total";
+  public const string music_box = "music_box";
+  public const string music_box_total = "music_box_total";
+  public const string nameplates = "nameplates";
+  public const string nameplates_total = "nameplates_total";
+  public const string loops_test_10 = "loops_test_10";
+  public const string loops_test_100 = "loops_test_100";
+  public const string loops_test_1000 = "loops_test_1000";
+  public const string loops_test_10000 = "loops_test_10000";
+  public const string loops_test_total_10 = "loops_test_total_10";
+  public const string loops_test_total_100 = "loops_test_total_100";
+  public const string loops_test_total_1000 = "loops_test_total_1000";
+  public const string loops_test_total_10000 = "loops_test_total_10000";
+  public const string dist_test = "dist_test";
+  public const string dist_test_total = "dist_test_total";
+  public const string shuffle_test_10 = "shuffle_test_10";
+  public const string shuffle_test_100 = "shuffle_test_100";
+  public const string shuffle_test_1000 = "shuffle_test_1000";
+  public const string shuffle_test_10000 = "shuffle_test_10000";
+  public const string shuffle_test_total_10 = "shuffle_test_total_10";
+  public const string shuffle_test_total_100 = "shuffle_test_total_100";
+  public const string shuffle_test_total_1000 = "shuffle_test_total_1000";
+  public const string shuffle_test_total_10000 = "shuffle_test_total_10000";
+  public const string loops_struct_test = "loops_struct_test";
+  public const string loops_struct_test_total = "loops_struct_test_total";
+  public const string decisions_test = "decisions_test";
+  public const string decisions_test_total = "decisions_test_total";
+  public const string ecs_test = "ecs_test";
+  public const string ecs_test_total = "ecs_test_total";
+  public const string blacklist_test = "blacklist_test";
+  public const string blacklist_test_total = "blacklist_test_total";
+  public const string actors = "actors";
+  public const string buildings = "buildings";
+  public const string effects_items = "effects_items";
+  public const string effects_traits = "effects_traits";
+  public const string loading = "loading";
+  public const string ai_actions = "ai_actions";
+  public const string ai_actions_total = "ai_actions_total";
+  public const string ai_tasks = "ai_tasks";
+  public const string ai_tasks_total = "ai_tasks_total";
+}
