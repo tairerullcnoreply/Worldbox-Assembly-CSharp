4dce0039f4ac5db5234540a142b826da30481060
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:04:53 -0500

    Add BlacklistTest6.cs

---
+++ BlacklistTest6.cs
@@ -0,0 +1,51 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BlacklistTest6
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+public class BlacklistTest6
+{
+  private static readonly Dictionary<char, char[][]> _profanity = new Dictionary<char, char[][]>();
+  private static readonly HashSet<char> _unique = new HashSet<char>();
+  private static bool _initiated = false;
+
+  public static void init()
+  {
+    if (BlacklistTest6._initiated)
+      return;
+    BlacklistTest6._initiated = true;
+    BlacklistTools.loadProfanityFilter(BlacklistTest6._profanity, BlacklistTest6._unique);
+  }
+
+  internal static bool checkBlackList(string pName)
+  {
+    string lower = pName.ToLower();
+    BlacklistTest6._unique.Clear();
+    BlacklistTest6._unique.UnionWith((IEnumerable<char>) lower);
+    BlacklistTest6._unique.RemoveWhere((Predicate<char>) (pChar => !char.IsLetter(pChar)));
+    string str = BlacklistTools.cleanStringAsSpan(lower);
+    bool flag = !(str == lower);
+    Dictionary<char, char[][]> profanity = BlacklistTest6._profanity;
+    ReadOnlySpan<char> pText1 = MemoryExtensions.AsSpan(lower);
+    ReadOnlySpan<char> pText2 = flag ? MemoryExtensions.AsSpan(str) : ReadOnlySpan<char>.op_Implicit((char[]) null);
+    foreach (char key in BlacklistTest6._unique)
+    {
+      char[][] chArray;
+      if (profanity.TryGetValue(key, out chArray))
+      {
+        for (int index = 0; index < chArray.Length; ++index)
+        {
+          ReadOnlySpan<char> pSearchPattern = Span<char>.op_Implicit(MemoryExtensions.AsSpan<char>(chArray[index]));
+          if (BlacklistTools.contains(pText1, pSearchPattern) || flag && BlacklistTools.contains(pText2, pSearchPattern))
+            return true;
+        }
+      }
+    }
+    return false;
+  }
+}
