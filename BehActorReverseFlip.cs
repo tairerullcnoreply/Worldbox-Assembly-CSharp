aac7b158edb7c054602749bdde27bb134bf5bf31
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:07 -0500

    Add BehActorReverseFlip.cs

---
+++ BehActorReverseFlip.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehActorReverseFlip
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class BehActorReverseFlip : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.setFlip(!pActor.flip);
+    return BehResult.Continue;
+  }
+}
