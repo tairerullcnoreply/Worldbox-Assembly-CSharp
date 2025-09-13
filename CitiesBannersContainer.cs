3d87772067b5c3537aef0cf5cfd869a3557d9041
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:06:32 -0500

    Add CitiesBannersContainer.cs

---
+++ CitiesBannersContainer.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CitiesBannersContainer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class CitiesBannersContainer : BannersMetaContainer<CityBanner, City, CityData>
+{
+  protected override IEnumerable<City> getMetaList(IMetaObject pMeta) => pMeta.getCities();
+}
