9fc3684cb85ca81555ec85ca435aa2c02db86cda
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:11:16 -0500

    Add GameStatsData.cs

---
+++ GameStatsData.cs
@@ -0,0 +1,59 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GameStatsData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+[Serializable]
+public class GameStatsData
+{
+  public double gameTime;
+  public long treesGrown;
+  public long floraGrown;
+  public long meteoritesLaunched;
+  public long pixelsExploded;
+  public long creaturesBorn;
+  public long creaturesCreated;
+  public long creaturesDied;
+  public long subspeciesCreated;
+  public long subspeciesExtinct;
+  public long languagesCreated;
+  public long languagesForgotten;
+  public long booksWritten;
+  public long booksRead;
+  public long booksBurnt;
+  public long culturesCreated;
+  public long culturesForgotten;
+  public long religionsCreated;
+  public long religionsForgotten;
+  public long kingdomsCreated;
+  public long kingdomsDestroyed;
+  public long citiesCreated;
+  public long citiesConquered;
+  public long citiesRebelled;
+  public long citiesDestroyed;
+  public long alliancesMade;
+  public long alliancesDissolved;
+  public long warsStarted;
+  public long peacesMade;
+  public long plotsStarted;
+  public long plotsSucceeded;
+  public long plotsForgotten;
+  public long familiesCreated;
+  public long familiesDestroyed;
+  public long clansCreated;
+  public long clansDestroyed;
+  public long armiesCreated;
+  public long armiesDestroyed;
+  public long creaturesSacrificed;
+  public long elvesSacrificed;
+  public long boatsDestroyedByMagnet;
+  public long gameLaunches;
+  public long bombsDropped;
+  public long wallsPlaced;
+  public long workshopUploads;
+  public long mapsCreated;
+}
