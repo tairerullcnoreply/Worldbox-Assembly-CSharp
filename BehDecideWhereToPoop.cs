64ac00de3639190b120c89dafb93cfc9d4289ee5
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:50 -0500

    Add BehDecideWhereToPoop.cs

---
+++ BehDecideWhereToPoop.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehDecideWhereToPoop
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class BehDecideWhereToPoop : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    return pActor.isAdult() && pActor.hasHouseCityInBordersAndSameIsland() ? this.forceTask(pActor, "poop_inside", false, true) : this.forceTask(pActor, "poop_outside", false, true);
+  }
+}
