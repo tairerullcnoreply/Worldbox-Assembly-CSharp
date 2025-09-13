1721f92a6ade9011a774878c1f09442c7b525546
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:39 -0500

    Add BehCheckPlot.cs

---
+++ BehCheckPlot.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehCheckPlot
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class BehCheckPlot : BehCheckPlotBase
+{
+  public override BehResult execute(Actor pActor)
+  {
+    if (!pActor.hasPlot())
+      return BehResult.Stop;
+    if (this.isBasePlotCheckOk(pActor))
+      return this.forceTask(pActor, "progress_plot");
+    pActor.leavePlot();
+    return BehResult.Stop;
+  }
+}
