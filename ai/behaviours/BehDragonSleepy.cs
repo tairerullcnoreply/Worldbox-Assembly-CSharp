7b7b638a445c5b7603b8c616fcd44eaf29efad7b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:17 -0500

    Add ai/behaviours/BehDragonSleepy.cs

---
+++ ai/behaviours/BehDragonSleepy.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehDragonSleepy
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehDragonSleepy : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    if (pActor.hasTrait("zombie"))
+      return BehResult.Continue;
+    int pResult;
+    pActor.data.get("sleepy", out pResult);
+    pActor.data.set("sleepy", ++pResult);
+    return BehResult.Continue;
+  }
+}
