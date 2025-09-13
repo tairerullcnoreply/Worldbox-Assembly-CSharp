bba74a44f140798189cd49af107293cb0bb7991d
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:29:11 -0500

    Add ai/behaviours/BehBoatDangerCheck.cs

---
+++ ai/behaviours/BehBoatDangerCheck.cs
@@ -0,0 +1,27 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehBoatDangerCheck
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehBoatDangerCheck : BehBoat
+{
+  public override BehResult execute(Actor pActor)
+  {
+    if (pActor.attackedBy == null)
+      return BehResult.Stop;
+    if ((double) pActor.getHealthRatio() < 0.25)
+    {
+      this.checkHomeDocks(pActor);
+      if (this.boat.actor.getHomeBuilding() != null)
+      {
+        pActor.cancelAllBeh();
+        return this.forceTask(pActor, "boat_return_to_dock");
+      }
+    }
+    return BehResult.Continue;
+  }
+}
