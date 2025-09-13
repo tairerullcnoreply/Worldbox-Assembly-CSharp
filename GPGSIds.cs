334c22bf9404b88372f2079136868978f6741421
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:11:48 -0500

    Add GPGSIds.cs

---
+++ GPGSIds.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GPGSIds
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class GPGSIds
+{
+  public const string achievement_10000_bombs = "CgkIia6M98wfEAIQBQ";
+  public const string achievement_megapolis = "CgkIia6M98wfEAIQBg";
+  public const string achievement_make_wilhelm_scream = "CgkIia6M98wfEAIQBw";
+  public const string achievement_1000_creatures = "CgkIia6M98wfEAIQBA";
+  public const string achievement_baby_tornado = "CgkIia6M98wfEAIQAw";
+  public const string achievement_lava_strike = "CgkIia6M98wfEAIQAg";
+}
