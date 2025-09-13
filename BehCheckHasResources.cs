e68f3e913863c2bb2dee331031c30a8ba9a9efa2
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:37 -0500

    Add BehCheckHasResources.cs

---
+++ BehCheckHasResources.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehCheckHasResources
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class BehCheckHasResources : BehaviourActionActor
+{
+  public override BehResult execute(Actor pObject)
+  {
+    return pObject.isCarryingResources() ? BehResult.Continue : BehResult.Stop;
+  }
+}
