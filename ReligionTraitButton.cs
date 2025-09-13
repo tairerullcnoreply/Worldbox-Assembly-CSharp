f1d0630670cefe93adba47fca18e884b67c73da1
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:20:32 -0500

    Add ReligionTraitButton.cs

---
+++ ReligionTraitButton.cs
@@ -0,0 +1,29 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ReligionTraitButton
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class ReligionTraitButton : TraitButton<ReligionTrait>
+{
+  internal override void load(string pTraitID)
+  {
+    this.load(AssetManager.religion_traits.get(pTraitID));
+  }
+
+  protected override void startSignal()
+  {
+    AchievementLibrary.trait_explorer_religion.checkBySignal();
+  }
+
+  protected override string tooltip_type => "religion_trait";
+
+  protected override TooltipData tooltipDataBuilder()
+  {
+    return new TooltipData()
+    {
+      religion_trait = this.augmentation_asset
+    };
+  }
+}
