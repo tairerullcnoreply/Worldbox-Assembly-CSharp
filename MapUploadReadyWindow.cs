4ebbe8abdd52445fed81faf63c37a7569198dc0b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:16:12 -0500

    Add MapUploadReadyWindow.cs

---
+++ MapUploadReadyWindow.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MapUploadReadyWindow
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#nullable disable
+public class MapUploadReadyWindow : MonoBehaviour
+{
+  public Button uploadButton;
+  public Text statusMessage;
+  public WorldElement worldElementPrefab;
+  public WorldElement element;
+  public Transform transformContent;
+}
