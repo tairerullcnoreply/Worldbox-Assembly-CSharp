1b3fbd4633e46309e8dfbea732b05a66b386c223
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:58 -0500

    Add TesterBehPickRandomRace.cs

---
+++ TesterBehPickRandomRace.cs
@@ -0,0 +1,33 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehPickRandomRace
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+using System.Collections.Generic;
+
+#nullable disable
+public class TesterBehPickRandomRace : BehaviourActionTester
+{
+  private static List<string> assets;
+
+  public TesterBehPickRandomRace()
+  {
+    if (TesterBehPickRandomRace.assets != null)
+      return;
+    TesterBehPickRandomRace.assets = new List<string>()
+    {
+      "human",
+      "elf",
+      "orc",
+      "dwarf"
+    };
+  }
+
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    pObject.beh_asset_target = TesterBehPickRandomRace.assets.GetRandom<string>();
+    return base.execute(pObject);
+  }
+}
