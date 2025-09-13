998a1e931aa6532fc0238c61672fad26f6d6ba2a
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:24:06 -0500

    Add TesterBehScreenshotFolder.cs

---
+++ TesterBehScreenshotFolder.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehScreenshotFolder
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+using System.IO;
+
+#nullable disable
+public class TesterBehScreenshotFolder : BehaviourActionTester
+{
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    string screenshotFolder = TesterBehScreenshotFolder.getScreenshotFolder(LocalizedTextManager.instance.language);
+    if (!Directory.Exists(screenshotFolder))
+      Directory.CreateDirectory(screenshotFolder);
+    return BehResult.Continue;
+  }
+
+  internal static string getScreenshotFolder(string pLanguage) => "GenAssets/Windows/" + pLanguage;
+}
