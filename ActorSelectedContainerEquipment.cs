bd7dbadd4b4766bf9e442acde60f53f6c56c1c09
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:00:44 -0500

    Add ActorSelectedContainerEquipment.cs

---
+++ ActorSelectedContainerEquipment.cs
@@ -0,0 +1,36 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ActorSelectedContainerEquipment
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class ActorSelectedContainerEquipment : SelectedElementBase<EquipmentButton>
+{
+  [SerializeField]
+  private EquipmentButton _prefab_equipment;
+
+  private void Awake()
+  {
+    this._pool = new ObjectPoolGenericMono<EquipmentButton>(this._prefab_equipment, this._grid);
+  }
+
+  public void update(Actor pActor)
+  {
+    if (!pActor.hasEquipment())
+      this.clear();
+    else
+      this.refresh((NanoObject) pActor);
+  }
+
+  protected override void refresh(NanoObject pNano)
+  {
+    this.clear();
+    foreach (Item pItem in ((Actor) pNano).equipment.getItems())
+      this.loadEquipmentButton(pItem);
+  }
+
+  private void loadEquipmentButton(Item pItem) => this._pool.getNext().load(pItem);
+}
