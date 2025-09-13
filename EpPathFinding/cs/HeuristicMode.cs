df4201b63c641146c84121abbb4b9e91be883317
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:32:48 -0500

    Add EpPathFinding/cs/HeuristicMode.cs

---
+++ EpPathFinding/cs/HeuristicMode.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EpPathFinding.cs.HeuristicMode
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace EpPathFinding.cs;
+
+public enum HeuristicMode
+{
+  MANHATTAN,
+  EUCLIDEAN,
+  CHEBYSHEV,
+}
