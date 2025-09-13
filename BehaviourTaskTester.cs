cccdba500ace9b41bcce1a0525608c010caf1d13
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:23 -0500

    Add BehaviourTaskTester.cs

---
+++ BehaviourTaskTester.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehaviourTaskTester
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+[Serializable]
+public class BehaviourTaskTester : BehaviourTaskBase<BehaviourActionTester>
+{
+  protected override bool has_locales => false;
+
+  protected override string locale_key_prefix => "task_tester";
+}
