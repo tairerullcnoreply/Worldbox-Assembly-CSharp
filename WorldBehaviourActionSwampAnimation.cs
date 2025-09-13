21d03910e54353a0c93890e496274d075ceda2b8
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:27:45 -0500

    Add WorldBehaviourActionSwampAnimation.cs

---
+++ WorldBehaviourActionSwampAnimation.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WorldBehaviourActionSwampAnimation
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class WorldBehaviourActionSwampAnimation
+{
+  public static void updateSwampTiles()
+  {
+    if (TopTileLibrary.swamp_low.hashset.Count < 10)
+      return;
+    World.world.redrawRenderedTile(TopTileLibrary.swamp_low.getCurrentTiles().GetRandom<WorldTile>());
+  }
+}
