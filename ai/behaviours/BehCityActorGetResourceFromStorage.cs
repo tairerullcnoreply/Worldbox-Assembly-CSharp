a7a8f0828986c8e2c69682fc2e52ba765c2ca974
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:29:55 -0500

    Add ai/behaviours/BehCityActorGetResourceFromStorage.cs

---
+++ ai/behaviours/BehCityActorGetResourceFromStorage.cs
@@ -0,0 +1,30 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehCityActorGetResourceFromStorage
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehCityActorGetResourceFromStorage : BehCityActor
+{
+  private string _resource_id;
+  private int _amount;
+
+  public BehCityActorGetResourceFromStorage(string pResourceId, int pAmount)
+  {
+    this._resource_id = pResourceId;
+    this._amount = pAmount;
+  }
+
+  public override BehResult execute(Actor pActor)
+  {
+    City city = pActor.city;
+    if (!city.hasStorages() || city.getResourcesAmount(this._resource_id) < this._amount)
+      return BehResult.Stop;
+    city.takeResource(this._resource_id, this._amount);
+    pActor.addToInventory(this._resource_id, this._amount);
+    return BehResult.Continue;
+  }
+}
