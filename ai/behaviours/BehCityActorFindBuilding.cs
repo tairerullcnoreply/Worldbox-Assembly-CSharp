6345de916083cc90f54de6ad78787dd37bc3413e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:29:46 -0500

    Add ai/behaviours/BehCityActorFindBuilding.cs

---
+++ ai/behaviours/BehCityActorFindBuilding.cs
@@ -0,0 +1,34 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehCityActorFindBuilding
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehCityActorFindBuilding : BehCityActor
+{
+  private string _type;
+  private string[] _types;
+  private bool _only_free_tile;
+
+  public BehCityActorFindBuilding(string pType, bool pFreeTile = true)
+  {
+    this._type = pType;
+    this._only_free_tile = pFreeTile;
+    if (!pType.Contains(","))
+      return;
+    this._types = pType.Split(',', StringSplitOptions.None);
+  }
+
+  public override BehResult execute(Actor pActor)
+  {
+    if (this._types != null)
+      this._type = this._types.GetRandom<string>();
+    pActor.beh_building_target = ActorTool.findNewBuildingTarget(pActor, this._type);
+    return BehResult.Continue;
+  }
+}
