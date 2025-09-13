3280567e97a9e58b75d9a6783dbe2823475dc24e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:01:43 -0500

    Add ArmiesColorsLibrary.cs

---
+++ ArmiesColorsLibrary.cs
@@ -0,0 +1,27 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ArmiesColorsLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class ArmiesColorsLibrary : ColorLibrary
+{
+  public ArmiesColorsLibrary() => this.file_path = "colors/colors_general";
+
+  public override void init()
+  {
+    base.init();
+    this.loadFromFile<ArmiesColorsLibrary>();
+  }
+
+  public override bool isColorUsedInWorld(ColorAsset pAsset)
+  {
+    foreach (Army army in (CoreSystemManager<Army, ArmyData>) World.world.armies)
+    {
+      if (this.checkColor(pAsset, army.data.color_id))
+        return true;
+    }
+    return base.isColorUsedInWorld(pAsset);
+  }
+}
