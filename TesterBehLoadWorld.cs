3a5c822bde8b6052fa2c6c7a51734b574b82a009
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:53 -0500

    Add TesterBehLoadWorld.cs

---
+++ TesterBehLoadWorld.cs
@@ -0,0 +1,30 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehLoadWorld
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class TesterBehLoadWorld : BehaviourActionTester
+{
+  private int slot;
+  private string fallback;
+
+  public TesterBehLoadWorld(int pSlot, string pFallback = null)
+  {
+    this.slot = pSlot;
+    this.fallback = pFallback;
+  }
+
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    SaveManager.setCurrentSlot(this.slot);
+    if (!SaveManager.currentSlotExists())
+      SaveManager.loadMapFromResources(this.fallback);
+    else
+      BehaviourActionBase<AutoTesterBot>.world.save_manager.startLoadSlot();
+    return BehResult.Continue;
+  }
+}
