b93b5dab01c67d8d8f02a31687d1702642011d95
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:24:04 -0500

    Add TesterBehRestartJobTest.cs

---
+++ TesterBehRestartJobTest.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehRestartJobTest
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class TesterBehRestartJobTest : BehaviourActionTester
+{
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    pObject.ai.restartJob();
+    return BehResult.Continue;
+  }
+}
