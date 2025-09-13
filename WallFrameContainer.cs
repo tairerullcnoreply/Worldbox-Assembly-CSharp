68f78998e569272d25c17c079acdb9b73e5dba72
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:26:47 -0500

    Add WallFrameContainer.cs

---
+++ WallFrameContainer.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WallFrameContainer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+internal class WallFrameContainer
+{
+  public Sprite[] sprites;
+}
