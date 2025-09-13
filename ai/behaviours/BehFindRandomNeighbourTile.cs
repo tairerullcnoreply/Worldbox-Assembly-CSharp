b7239a87541d1b7f51b8c59e340330b2665c1fbf
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:40 -0500

    Add ai/behaviours/BehFindRandomNeighbourTile.cs

---
+++ ai/behaviours/BehFindRandomNeighbourTile.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehFindRandomNeighbourTile
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehFindRandomNeighbourTile : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    WorldTile walkableTileAround = pActor.current_tile.getWalkableTileAround(pActor.current_tile);
+    pActor.beh_tile_target = walkableTileAround;
+    return walkableTileAround == null ? BehResult.Stop : BehResult.Continue;
+  }
+}
