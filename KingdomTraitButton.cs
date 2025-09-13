4499428ccedbb2b76e3758adbf3f81efbb5073e1
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:14:18 -0500

    Add KingdomTraitButton.cs

---
+++ KingdomTraitButton.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: KingdomTraitButton
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class KingdomTraitButton : TraitButton<KingdomTrait>
+{
+  internal override void load(string pTraitID)
+  {
+    this.load(AssetManager.kingdoms_traits.get(pTraitID));
+  }
+
+  protected override string tooltip_type => "kingdom_trait";
+
+  protected override TooltipData tooltipDataBuilder()
+  {
+    return new TooltipData()
+    {
+      kingdom_trait = this.augmentation_asset
+    };
+  }
+}
