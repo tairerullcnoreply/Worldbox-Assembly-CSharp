ac7acb923491567a8807640ce769e99331decc7c
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:05:51 -0500

    Add BuildingType.cs

---
+++ BuildingType.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BuildingType
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum BuildingType
+{
+  Building_None,
+  Building_Tree,
+  Building_Fruits,
+  Building_Hives,
+  Building_Poops,
+  Building_Wheat,
+  Building_Plant,
+  Building_Mineral,
+  Building_Nature,
+  Building_Mob,
+  Building_Creep,
+  Building_Civ,
+}
