bead5b0d97d83b0430303fa9d9ca4d7faa1e06e2
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:07:01 -0500

    Add CityStorageSlot.cs

---
+++ CityStorageSlot.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CityStorageSlot
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using Newtonsoft.Json;
+using System;
+
+#nullable disable
+[Serializable]
+public class CityStorageSlot
+{
+  public string id;
+  public int amount;
+
+  public CityStorageSlot(string pID) => this.create(pID);
+
+  public void create(string pID) => this.id = pID;
+
+  [JsonIgnore]
+  public ResourceAsset asset => AssetManager.resources.get(this.id);
+}
