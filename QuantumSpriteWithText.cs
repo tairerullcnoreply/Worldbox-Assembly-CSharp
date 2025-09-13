b4df69711a8a10246267ebbabd0c5bab0be81f1c
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:19:57 -0500

    Add QuantumSpriteWithText.cs

---
+++ QuantumSpriteWithText.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: QuantumSpriteWithText
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class QuantumSpriteWithText : QuantumSprite
+{
+  public TextMesh text;
+
+  public void initText()
+  {
+    this.text = ((Component) ((Component) this).transform.Find("Text"))?.GetComponent<TextMesh>();
+  }
+}
