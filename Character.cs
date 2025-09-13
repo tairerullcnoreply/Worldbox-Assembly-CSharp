7888292b983db8120f242016896b0d368c5498ce
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:06:24 -0500

    Add Character.cs

---
+++ Character.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Character
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class Character : ScriptableObject
+{
+  public string name_first;
+  public string name_last;
+  public string name_nickname;
+  public int age;
+  public object religion;
+  public object preferences;
+  public object perks;
+}
