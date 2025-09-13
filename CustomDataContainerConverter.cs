c7fae2cf4932d7e60d842f6a53ee6e61a85c026e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:08:41 -0500

    Add CustomDataContainerConverter.cs

---
+++ CustomDataContainerConverter.cs
@@ -0,0 +1,36 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CustomDataContainerConverter
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using Newtonsoft.Json;
+using System;
+using System.Reflection;
+
+#nullable disable
+public class CustomDataContainerConverter : JsonConverter
+{
+  public virtual void WriteJson(JsonWriter pWriter, object pValue, JsonSerializer pSerializer)
+  {
+    FieldInfo field = pValue.GetType().GetField("dict", BindingFlags.Instance | BindingFlags.NonPublic);
+    Type fieldType = field.FieldType;
+    object obj = field.GetValue(pValue);
+    pSerializer.Serialize(pWriter, obj, fieldType);
+  }
+
+  public virtual object ReadJson(
+    JsonReader pReader,
+    Type pObjectType,
+    object pExistingValue,
+    JsonSerializer pSerializer)
+  {
+    object instance = Activator.CreateInstance(pObjectType);
+    FieldInfo field = pObjectType.GetField("dict", BindingFlags.Instance | BindingFlags.NonPublic);
+    Type fieldType = field.FieldType;
+    field.SetValue(instance, pSerializer.Deserialize(pReader, fieldType));
+    return instance;
+  }
+
+  public virtual bool CanConvert(Type pObjectType) => false;
+}
