1a8ddc7d4768663a81b817c969b57337eda0ee42
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:14:39 -0500

    Add LanguageSelectedContainerTraits.cs

---
+++ LanguageSelectedContainerTraits.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: LanguageSelectedContainerTraits
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class LanguageSelectedContainerTraits : 
+  SelectedContainerTraits<LanguageTrait, LanguageTraitButton, LanguageTraitsContainer, LanguageTraitsEditor>
+{
+  protected override MetaType meta_type => MetaType.Language;
+
+  protected override IReadOnlyCollection<LanguageTrait> getTraits()
+  {
+    return SelectedMetas.selected_language.getTraits();
+  }
+
+  protected override bool canEditTraits() => true;
+}
