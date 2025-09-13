475d8f02a5eb2f594176c98f471299dfe4ed327d
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:01:02 -0500

    Add ActorTraitGroupLibrary.cs

---
+++ ActorTraitGroupLibrary.cs
@@ -0,0 +1,89 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ActorTraitGroupLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class ActorTraitGroupLibrary : BaseCategoryLibrary<ActorTraitGroupAsset>
+{
+  public override void init()
+  {
+    base.init();
+    ActorTraitGroupAsset pAsset1 = new ActorTraitGroupAsset();
+    pAsset1.id = "cognitive";
+    pAsset1.name = "trait_group_cognitive";
+    pAsset1.color = "#5EFFFF";
+    this.add(pAsset1);
+    ActorTraitGroupAsset pAsset2 = new ActorTraitGroupAsset();
+    pAsset2.id = "mind";
+    pAsset2.name = "trait_group_mind";
+    pAsset2.color = "#BAF0F4";
+    this.add(pAsset2);
+    ActorTraitGroupAsset pAsset3 = new ActorTraitGroupAsset();
+    pAsset3.id = "spirit";
+    pAsset3.name = "trait_group_spirit";
+    pAsset3.color = "#BC42FF";
+    this.add(pAsset3);
+    ActorTraitGroupAsset pAsset4 = new ActorTraitGroupAsset();
+    pAsset4.id = "physique";
+    pAsset4.name = "trait_group_physique";
+    pAsset4.color = "#FF6145";
+    this.add(pAsset4);
+    ActorTraitGroupAsset pAsset5 = new ActorTraitGroupAsset();
+    pAsset5.id = "health";
+    pAsset5.name = "trait_group_health";
+    pAsset5.color = "#89FF56";
+    this.add(pAsset5);
+    ActorTraitGroupAsset pAsset6 = new ActorTraitGroupAsset();
+    pAsset6.id = "body";
+    pAsset6.name = "trait_group_body";
+    pAsset6.color = "#FF6B86";
+    this.add(pAsset6);
+    ActorTraitGroupAsset pAsset7 = new ActorTraitGroupAsset();
+    pAsset7.id = "appearance";
+    pAsset7.name = "trait_group_appearance";
+    pAsset7.color = "#FF6DEB";
+    this.add(pAsset7);
+    ActorTraitGroupAsset pAsset8 = new ActorTraitGroupAsset();
+    pAsset8.id = "protection";
+    pAsset8.name = "trait_group_protection";
+    pAsset8.color = "#FF6B86";
+    this.add(pAsset8);
+    ActorTraitGroupAsset pAsset9 = new ActorTraitGroupAsset();
+    pAsset9.id = "skills";
+    pAsset9.name = "trait_group_skills";
+    pAsset9.color = "#BCBCBC";
+    this.add(pAsset9);
+    ActorTraitGroupAsset pAsset10 = new ActorTraitGroupAsset();
+    pAsset10.id = "merits";
+    pAsset10.name = "trait_group_merits";
+    pAsset10.color = "#FFDA23";
+    this.add(pAsset10);
+    ActorTraitGroupAsset pAsset11 = new ActorTraitGroupAsset();
+    pAsset11.id = "acquired";
+    pAsset11.name = "trait_group_acquired";
+    pAsset11.color = "#A3AFFF";
+    this.add(pAsset11);
+    ActorTraitGroupAsset pAsset12 = new ActorTraitGroupAsset();
+    pAsset12.id = "fun";
+    pAsset12.name = "trait_group_fun";
+    pAsset12.color = "#FFFAA3";
+    this.add(pAsset12);
+    ActorTraitGroupAsset pAsset13 = new ActorTraitGroupAsset();
+    pAsset13.id = "fate";
+    pAsset13.name = "trait_group_fate";
+    pAsset13.color = "#ffd82f";
+    this.add(pAsset13);
+    ActorTraitGroupAsset pAsset14 = new ActorTraitGroupAsset();
+    pAsset14.id = "miscellaneous";
+    pAsset14.name = "trait_group_miscellaneous";
+    pAsset14.color = "#D8D8D8";
+    this.add(pAsset14);
+    ActorTraitGroupAsset pAsset15 = new ActorTraitGroupAsset();
+    pAsset15.id = "special";
+    pAsset15.name = "trait_group_special";
+    pAsset15.color = "#FF8F44";
+    this.add(pAsset15);
+  }
+}
