08c8391e213e5c1c12ccc0ad58051972f4e6b1df
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:09:27 -0500

    Add DeleteWorldButton.cs

---
+++ DeleteWorldButton.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DeleteWorldButton
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+#nullable disable
+public class DeleteWorldButton : MonoBehaviour
+{
+  private void Start()
+  {
+    Button button;
+    if (!((Component) this).TryGetComponent<Button>(ref button))
+      return;
+    // ISSUE: method pointer
+    ((UnityEvent) button.onClick).AddListener(new UnityAction((object) this, __methodptr(deleteWorld)));
+  }
+
+  private void deleteWorld() => ScrollWindow.showWindow("save_delete_confirm");
+}
