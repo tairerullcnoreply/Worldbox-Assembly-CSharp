1d529610fbaf74ffe17322cf8ba12e83a25e8b44
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:28:20 -0500

    Add WorldTileZoneBorder.cs

---
+++ WorldTileZoneBorder.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WorldTileZoneBorder
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class WorldTileZoneBorder
+{
+  internal bool corner_l_u;
+  internal bool corner_l_d;
+  internal bool corner_r_d;
+  internal bool corner_r_u;
+  internal bool border_up;
+  internal bool border_left;
+  internal bool border_right;
+  internal bool border_down;
+  internal bool border;
+
+  internal void reset()
+  {
+  }
+}
