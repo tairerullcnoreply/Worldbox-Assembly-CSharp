665114cb5ea457eb09d2941e7a6e3c24f058bd31
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:01:47 -0500

    Add ArmyElement.cs

---
+++ ArmyElement.cs
@@ -0,0 +1,11 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ArmyElement
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class ArmyElement : WindowMetaElement<Army, ArmyData>
+{
+  protected Army army => this.meta_object;
+}
