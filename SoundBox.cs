56d3c808c6e3b54bf8513f09b07df0f99fcfa4f8
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:22:13 -0500

    Add SoundBox.cs

---
+++ SoundBox.cs
@@ -0,0 +1,11 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SoundBox
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class SoundBox
+{
+  public static void click() => MusicBox.playSoundUI("event:/SFX/Ui/Click");
+}
