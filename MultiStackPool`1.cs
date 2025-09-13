9c8285a4f2a834930001739f6693d0e6e9e2ace5
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:17:11 -0500

    Add MultiStackPool`1.cs

---
+++ MultiStackPool`1.cs
@@ -0,0 +1,41 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MultiStackPool`1
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+public class MultiStackPool<T> where T : new()
+{
+  private Dictionary<Type, StackPool<T>> _pools = new Dictionary<Type, StackPool<T>>();
+
+  public U get<U>() where U : T, new()
+  {
+    Type key = typeof (U);
+    StackPool<T> stackPool;
+    if (!this._pools.TryGetValue(key, out stackPool))
+    {
+      stackPool = new StackPool<T>();
+      this._pools.Add(key, stackPool);
+    }
+    return stackPool.get<U>();
+  }
+
+  public void release(T pObject)
+  {
+    StackPool<T> stackPool;
+    if (!this._pools.TryGetValue(pObject.GetType(), out stackPool))
+      return;
+    stackPool.release(pObject);
+  }
+
+  public void clear()
+  {
+    foreach (StackPool<T> stackPool in this._pools.Values)
+      stackPool.clear();
+    this._pools.Clear();
+  }
+}
