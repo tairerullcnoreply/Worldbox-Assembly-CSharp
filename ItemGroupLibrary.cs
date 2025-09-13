d4eafea926ef529b186a2fa3e8461017990ffa06
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:13:31 -0500

    Add ItemGroupLibrary.cs

---
+++ ItemGroupLibrary.cs
@@ -0,0 +1,74 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ItemGroupLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class ItemGroupLibrary : BaseCategoryLibrary<ItemGroupAsset>
+{
+  public override void init()
+  {
+    base.init();
+    ItemGroupAsset pAsset1 = new ItemGroupAsset();
+    pAsset1.id = "helmet";
+    pAsset1.name = "equipment_group_helmet";
+    pAsset1.color = "#BAFFC2";
+    this.add(pAsset1);
+    ItemGroupAsset pAsset2 = new ItemGroupAsset();
+    pAsset2.id = "armor";
+    pAsset2.name = "equipment_group_armor";
+    pAsset2.color = "#BAFFC2";
+    this.add(pAsset2);
+    ItemGroupAsset pAsset3 = new ItemGroupAsset();
+    pAsset3.id = "boots";
+    pAsset3.name = "equipment_group_boots";
+    pAsset3.color = "#BAFFC2";
+    this.add(pAsset3);
+    ItemGroupAsset pAsset4 = new ItemGroupAsset();
+    pAsset4.id = "ring";
+    pAsset4.name = "equipment_group_ring";
+    pAsset4.color = "#BAFFC2";
+    this.add(pAsset4);
+    ItemGroupAsset pAsset5 = new ItemGroupAsset();
+    pAsset5.id = "amulet";
+    pAsset5.name = "equipment_group_amulet";
+    pAsset5.color = "#BAFFC2";
+    this.add(pAsset5);
+    ItemGroupAsset pAsset6 = new ItemGroupAsset();
+    pAsset6.id = "sword";
+    pAsset6.name = "equipment_group_sword";
+    pAsset6.color = "#BAFFC2";
+    this.add(pAsset6);
+    ItemGroupAsset pAsset7 = new ItemGroupAsset();
+    pAsset7.id = "axe";
+    pAsset7.name = "equipment_group_axe";
+    pAsset7.color = "#BAFFC2";
+    this.add(pAsset7);
+    ItemGroupAsset pAsset8 = new ItemGroupAsset();
+    pAsset8.id = "hammer";
+    pAsset8.name = "equipment_group_hammer";
+    pAsset8.color = "#BAFFC2";
+    this.add(pAsset8);
+    ItemGroupAsset pAsset9 = new ItemGroupAsset();
+    pAsset9.id = "spear";
+    pAsset9.name = "equipment_group_spear";
+    pAsset9.color = "#BAFFC2";
+    this.add(pAsset9);
+    ItemGroupAsset pAsset10 = new ItemGroupAsset();
+    pAsset10.id = "bow";
+    pAsset10.name = "equipment_group_bow";
+    pAsset10.color = "#BAFFC2";
+    this.add(pAsset10);
+    ItemGroupAsset pAsset11 = new ItemGroupAsset();
+    pAsset11.id = "staff";
+    pAsset11.name = "equipment_group_staff";
+    pAsset11.color = "#BAFFC2";
+    this.add(pAsset11);
+    ItemGroupAsset pAsset12 = new ItemGroupAsset();
+    pAsset12.id = "firearm";
+    pAsset12.name = "equipment_group_firearm";
+    pAsset12.color = "#BAFFC2";
+    this.add(pAsset12);
+  }
+}
