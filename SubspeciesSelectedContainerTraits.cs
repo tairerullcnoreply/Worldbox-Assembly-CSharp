569816ce5ff25792a6ba8c62ae05f070df202221
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:08 -0500

    Add SubspeciesSelectedContainerTraits.cs

---
+++ SubspeciesSelectedContainerTraits.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SubspeciesSelectedContainerTraits
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class SubspeciesSelectedContainerTraits : 
+  SelectedContainerTraits<SubspeciesTrait, SubspeciesTraitButton, SubspeciesTraitsContainer, SubspeciesTraitsEditor>
+{
+  protected override MetaType meta_type => MetaType.Subspecies;
+
+  protected override IReadOnlyCollection<SubspeciesTrait> getTraits()
+  {
+    return SelectedMetas.selected_subspecies.getTraits();
+  }
+
+  protected override bool canEditTraits() => true;
+}
