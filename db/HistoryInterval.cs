f4fa5f6baa3aad027aa20b99efdfedeb1be872e5
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:32:35 -0500

    Add db/HistoryInterval.cs

---
+++ db/HistoryInterval.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: db.HistoryInterval
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace db;
+
+public enum HistoryInterval
+{
+  None,
+  Yearly1,
+  Yearly5,
+  Yearly10,
+  Yearly50,
+  Yearly100,
+  Yearly500,
+  Yearly1000,
+  Yearly5000,
+  Yearly10000,
+}
