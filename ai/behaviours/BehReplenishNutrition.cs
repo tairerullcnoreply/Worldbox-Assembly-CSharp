17f1e1a718427453799fc5b2466e3e23ac60b340
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:31:29 -0500

    Add ai/behaviours/BehReplenishNutrition.cs

---
+++ ai/behaviours/BehReplenishNutrition.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehReplenishNutrition
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehReplenishNutrition : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.addNutritionFromEating(pActor.getMaxNutrition(), pSetJustAte: true);
+    pActor.countConsumed();
+    return BehResult.Continue;
+  }
+}
