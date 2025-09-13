6e73b65eae8def19cc7b453766be1eb1bb89e589
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:17:07 -0500

    Add MoodAsset.cs

---
+++ MoodAsset.cs
@@ -0,0 +1,27 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MoodAsset
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using Beebyte.Obfuscator;
+using System;
+using UnityEngine;
+
+#nullable disable
+[ObfuscateLiterals]
+[Serializable]
+public class MoodAsset : Asset
+{
+  public BaseStats base_stats = new BaseStats();
+  public string icon;
+  [NonSerialized]
+  public Sprite sprite;
+
+  public Sprite getSprite()
+  {
+    if (Object.op_Equality((Object) this.sprite, (Object) null))
+      this.sprite = SpriteTextureLoader.getSprite("ui/Icons/" + this.icon);
+    return this.sprite;
+  }
+}
