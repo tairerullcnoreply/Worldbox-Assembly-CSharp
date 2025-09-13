2c853d601348b83021548303ef12ea7b5a617837
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:02:25 -0500

    Add BannerAsset.cs

---
+++ BannerAsset.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BannerAsset
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+[Serializable]
+public class BannerAsset : Asset
+{
+  public List<string> backgrounds;
+  public List<string> icons;
+  public List<string> frames;
+}
