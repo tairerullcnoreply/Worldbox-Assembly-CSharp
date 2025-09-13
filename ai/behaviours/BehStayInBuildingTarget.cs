0c43ea5bdd8ba821210b5650599194a195d6b3b8
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:31:42 -0500

    Add ai/behaviours/BehStayInBuildingTarget.cs

---
+++ ai/behaviours/BehStayInBuildingTarget.cs
@@ -0,0 +1,36 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehStayInBuildingTarget
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehStayInBuildingTarget : BehCityActor
+{
+  private float min;
+  private float max;
+
+  public BehStayInBuildingTarget(float pMin = 0.0f, float pMax = 1f)
+  {
+    this.min = pMin;
+    this.max = pMax;
+  }
+
+  protected override void setupErrorChecks()
+  {
+    base.setupErrorChecks();
+    this.check_building_target_non_usable = true;
+    this.null_check_building_target = true;
+  }
+
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.timer_action = Randy.randomFloat(this.min, this.max);
+    pActor.stayInBuilding(pActor.beh_building_target);
+    pActor.beh_tile_target = (WorldTile) null;
+    pActor.beh_building_target.startShake(0.5f);
+    return BehResult.Continue;
+  }
+}
