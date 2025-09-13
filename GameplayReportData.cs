2e5a1ca280c4749a5780055e83cdf23774024a12
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:11:11 -0500

    Add GameplayReportData.cs

---
+++ GameplayReportData.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GameplayReportData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+internal class GameplayReportData
+{
+  public int usages_actors;
+  public int usages_biomes;
+  public int usages_food;
+  public List<string> owners = new List<string>();
+  public List<string> biomes = new List<string>();
+  public List<string> food = new List<string>();
+}
