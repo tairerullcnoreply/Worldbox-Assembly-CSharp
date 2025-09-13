868286f8e7c914506c95722f83d790890bcaf923
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:11:58 -0500

    Add GraphTimeScale.cs

---
+++ GraphTimeScale.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GraphTimeScale
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum GraphTimeScale
+{
+  year_10,
+  year_25,
+  year_100,
+  year_250,
+  year_500,
+  year_1000,
+  year_2500,
+  year_5000,
+  year_10000,
+  year_50000,
+  year_100000,
+}
