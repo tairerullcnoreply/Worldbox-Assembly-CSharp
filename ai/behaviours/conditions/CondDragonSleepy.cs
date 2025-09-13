95166488488c5f65b5f45166e5a916f5b3bb4746
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:32:28 -0500

    Add ai/behaviours/conditions/CondDragonSleepy.cs

---
+++ ai/behaviours/conditions/CondDragonSleepy.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.conditions.CondDragonSleepy
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours.conditions;
+
+public class CondDragonSleepy : BehaviourActorCondition
+{
+  public override bool check(Actor pActor)
+  {
+    int pResult;
+    pActor.data.get("sleepy", out pResult);
+    return pResult > 10;
+  }
+}
