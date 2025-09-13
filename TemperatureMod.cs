8257b286c5d5139e886b518ed5509b661cc50320
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:33 -0500

    Add TemperatureMod.cs

---
+++ TemperatureMod.cs
@@ -0,0 +1,12 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TemperatureMod
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+internal readonly struct TemperatureMod(Actor pActor, int pNewTemperature)
+{
+  public readonly Actor actor = pActor;
+  public readonly int new_temperature = pNewTemperature;
+}
