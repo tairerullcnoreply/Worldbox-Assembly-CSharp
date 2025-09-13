48dc8ba4ba006ba3213e5be463050d8b19fb34ad
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:25:16 -0500

    Add TooltipKingdomTraitsRow.cs

---
+++ TooltipKingdomTraitsRow.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TooltipKingdomTraitsRow
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class TooltipKingdomTraitsRow : TooltipTraitsRow<KingdomTrait>
+{
+  protected override IReadOnlyCollection<KingdomTrait> traits_hashset
+  {
+    get => this.tooltip_data.kingdom.getTraits();
+  }
+}
