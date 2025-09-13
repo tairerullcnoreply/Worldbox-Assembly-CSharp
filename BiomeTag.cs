be8068488ce23e8b2e62986bbd2aa8e8ea4aaeb4
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:04:44 -0500

    Add BiomeTag.cs

---
+++ BiomeTag.cs
@@ -0,0 +1,38 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BiomeTag
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum BiomeTag
+{
+  Green,
+  Savanna,
+  Enchanted,
+  Corrupted,
+  Mushroom,
+  Swamp,
+  Jungle,
+  Desert,
+  Rocklands,
+  Crystal,
+  Infernal,
+  Candy,
+  Lemon,
+  Permafrost,
+  Birch,
+  Maple,
+  Flower,
+  Wasteland,
+  Garlic,
+  Soil,
+  Field,
+  Sand,
+  Solitude,
+  Paradox,
+  Clover,
+  Singularity,
+  Celestial,
+  Hills,
+}
