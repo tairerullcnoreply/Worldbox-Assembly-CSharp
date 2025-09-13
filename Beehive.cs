5691a361edf587e6ec4785f96116b33ac4c55076
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:02 -0500

    Add Beehive.cs

---
+++ Beehive.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Beehive
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class Beehive : BaseBuildingComponent
+{
+  public int honey;
+
+  public void addHoney()
+  {
+    if (this.honey >= 10)
+      return;
+    ++this.honey;
+    if (this.honey != 10)
+      return;
+    this.building.setHaveResourcesToCollect(true);
+  }
+}
