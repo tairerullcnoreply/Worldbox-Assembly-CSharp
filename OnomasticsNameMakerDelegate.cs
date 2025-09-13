c558517fe70a103d65b95855bfc8c7e9c1e115ea
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:18:10 -0500

    Add OnomasticsNameMakerDelegate.cs

---
+++ OnomasticsNameMakerDelegate.cs
@@ -0,0 +1,14 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: OnomasticsNameMakerDelegate
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public delegate string OnomasticsNameMakerDelegate(
+  OnomasticsAsset pAsset,
+  OnomasticsData pData,
+  StringBuilderPool pStringBuilder,
+  string pLastPart,
+  int pIndex,
+  ActorSex pSex);
