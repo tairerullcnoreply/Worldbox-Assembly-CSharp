bd31069e095856720b0a6014715b11ebc5905690
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:31:01 -0500

    Add ai/behaviours/BehFingerWaitForFlying.cs

---
+++ ai/behaviours/BehFingerWaitForFlying.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehFingerWaitForFlying
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehFingerWaitForFlying : BehFinger
+{
+  public override BehResult execute(Actor pActor)
+  {
+    return (double) this.finger.flying_target != (double) pActor.position_height ? BehResult.RepeatStep : BehResult.Continue;
+  }
+}
