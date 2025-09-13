9c7fdfa8ae3f3fd00c683ec19930a648718877d9
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:13:18 -0500

    Add ISapientListComponent.cs

---
+++ ISapientListComponent.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ISapientListComponent
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine.UI;
+
+#nullable disable
+public interface ISapientListComponent
+{
+  void setSapientCounter(Text pCounter);
+
+  void setNonSapientCounter(Text pCounter);
+
+  void setShowSapientOnly();
+
+  void setShowNonSapientOnly();
+
+  void setDefault();
+}
