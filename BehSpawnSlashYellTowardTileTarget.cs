dd1fb33f3cb9c6fca91c7e1ef7c863f6b78fbe9b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:04:22 -0500

    Add BehSpawnSlashYellTowardTileTarget.cs

---
+++ BehSpawnSlashYellTowardTileTarget.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehSpawnSlashYellTowardTileTarget
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+using UnityEngine;
+
+#nullable disable
+public class BehSpawnSlashYellTowardTileTarget : BehaviourActionActor
+{
+  protected override void setupErrorChecks()
+  {
+    base.setupErrorChecks();
+    this.null_check_tile_target = true;
+  }
+
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.spawnSlashYell(Vector2Int.op_Implicit(pActor.beh_tile_target.pos));
+    return BehResult.Continue;
+  }
+}
