0eb42ec5756cc7d27b477555469b07eb1a4ee4bb
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:28:02 -0500

    Add WorldLayer.cs

---
+++ WorldLayer.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WorldLayer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class WorldLayer : MapLayer
+{
+  public override void update(float pElapsed)
+  {
+  }
+
+  public override void draw(float pElapsed) => this.UpdateDirty(pElapsed);
+}
