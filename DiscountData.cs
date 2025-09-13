ee392d8602efacbdd648ce7f3362c5c26548a602
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:09:39 -0500

    Add DiscountData.cs

---
+++ DiscountData.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DiscountData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using Beebyte.Obfuscator;
+
+#nullable disable
+[Skip]
+[SkipRename]
+public struct DiscountData
+{
+  public string discount;
+  public string price_current;
+  public string price_old;
+}
