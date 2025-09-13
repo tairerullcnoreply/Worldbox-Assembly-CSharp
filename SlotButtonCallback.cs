bd76f68d39104e0744e24b8dd6704eda0ffb27e1
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:22:04 -0500

    Add SlotButtonCallback.cs

---
+++ SlotButtonCallback.cs
@@ -0,0 +1,14 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SlotButtonCallback
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class SlotButtonCallback : MonoBehaviour
+{
+  public int slotID;
+  public bool premiumOnly = true;
+}
