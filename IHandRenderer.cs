edadc5ac70c43457cb7ff98cf47ebf0aefbba9f3
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:12:53 -0500

    Add IHandRenderer.cs

---
+++ IHandRenderer.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: IHandRenderer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public interface IHandRenderer
+{
+  Sprite[] getSprites();
+
+  bool is_colored { get; }
+
+  bool is_animated { get; }
+
+  string getID() => (this as Asset).id;
+}
