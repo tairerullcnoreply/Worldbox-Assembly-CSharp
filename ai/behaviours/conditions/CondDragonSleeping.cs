d12198d52a2a81121b9cb943b0094557e84112fc
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:32:27 -0500

    Add ai/behaviours/conditions/CondDragonSleeping.cs

---
+++ ai/behaviours/conditions/CondDragonSleeping.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.conditions.CondDragonSleeping
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours.conditions;
+
+public class CondDragonSleeping : BehaviourActorCondition
+{
+  public override bool check(Actor pActor)
+  {
+    string id = pActor.ai.task?.id;
+    return id == "dragon_sleep" || id == "dragon_wakeup";
+  }
+}
