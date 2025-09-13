8d958d48601535bc19d1d11838ecb88503fc4c37
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:14 -0500

    Add SubspeciesTraitGroupLibrary.cs

---
+++ SubspeciesTraitGroupLibrary.cs
@@ -0,0 +1,119 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SubspeciesTraitGroupLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class SubspeciesTraitGroupLibrary : BaseCategoryLibrary<SubspeciesTraitGroupAsset>
+{
+  public override void init()
+  {
+    base.init();
+    SubspeciesTraitGroupAsset pAsset1 = new SubspeciesTraitGroupAsset();
+    pAsset1.id = "harmony";
+    pAsset1.name = "trait_group_harmony";
+    pAsset1.color = "#FFFAA3";
+    this.add(pAsset1);
+    SubspeciesTraitGroupAsset pAsset2 = new SubspeciesTraitGroupAsset();
+    pAsset2.id = "advanced_brain";
+    pAsset2.name = "trait_group_advanced_brain";
+    pAsset2.color = "#BAF0F4";
+    this.add(pAsset2);
+    SubspeciesTraitGroupAsset pAsset3 = new SubspeciesTraitGroupAsset();
+    pAsset3.id = "mind";
+    pAsset3.name = "trait_group_mind";
+    pAsset3.color = "#BAF0F4";
+    this.add(pAsset3);
+    SubspeciesTraitGroupAsset pAsset4 = new SubspeciesTraitGroupAsset();
+    pAsset4.id = "body";
+    pAsset4.name = "trait_group_body";
+    pAsset4.color = "#FF6B86";
+    this.add(pAsset4);
+    SubspeciesTraitGroupAsset pAsset5 = new SubspeciesTraitGroupAsset();
+    pAsset5.id = "diet";
+    pAsset5.name = "trait_group_diet";
+    pAsset5.color = "#42FF61";
+    this.add(pAsset5);
+    SubspeciesTraitGroupAsset pAsset6 = new SubspeciesTraitGroupAsset();
+    pAsset6.id = "rebirth";
+    pAsset6.name = "trait_group_rebirth";
+    pAsset6.color = "#FF6A00";
+    this.add(pAsset6);
+    SubspeciesTraitGroupAsset pAsset7 = new SubspeciesTraitGroupAsset();
+    pAsset7.id = "growth";
+    pAsset7.name = "trait_group_growth";
+    pAsset7.color = "#BAFFC2";
+    this.add(pAsset7);
+    SubspeciesTraitGroupAsset pAsset8 = new SubspeciesTraitGroupAsset();
+    pAsset8.id = "bioproducts";
+    pAsset8.name = "trait_group_bioproducts";
+    pAsset8.color = "#BAFFC2";
+    this.add(pAsset8);
+    SubspeciesTraitGroupAsset pAsset9 = new SubspeciesTraitGroupAsset();
+    pAsset9.id = "chaos";
+    pAsset9.name = "trait_group_chaos";
+    pAsset9.color = "#FF6B86";
+    this.add(pAsset9);
+    SubspeciesTraitGroupAsset pAsset10 = new SubspeciesTraitGroupAsset();
+    pAsset10.id = "talents";
+    pAsset10.name = "trait_group_talents";
+    pAsset10.color = "#BAF0F4";
+    this.add(pAsset10);
+    SubspeciesTraitGroupAsset pAsset11 = new SubspeciesTraitGroupAsset();
+    pAsset11.id = "sleep_cycles";
+    pAsset11.name = "trait_group_sleep_cycles";
+    pAsset11.color = "#FFFFFF";
+    this.add(pAsset11);
+    SubspeciesTraitGroupAsset pAsset12 = new SubspeciesTraitGroupAsset();
+    pAsset12.id = "hibernation";
+    pAsset12.name = "trait_group_hibernation";
+    pAsset12.color = "#FFFFFF";
+    this.add(pAsset12);
+    SubspeciesTraitGroupAsset pAsset13 = new SubspeciesTraitGroupAsset();
+    pAsset13.id = "reproduction_strategy";
+    pAsset13.name = "trait_group_reproduction_strategy";
+    pAsset13.color = "#FFA5E7";
+    this.add(pAsset13);
+    SubspeciesTraitGroupAsset pAsset14 = new SubspeciesTraitGroupAsset();
+    pAsset14.id = "reproductive_methods";
+    pAsset14.name = "trait_group_reproductive_methods";
+    pAsset14.color = "#FF83FF";
+    this.add(pAsset14);
+    SubspeciesTraitGroupAsset pAsset15 = new SubspeciesTraitGroupAsset();
+    pAsset15.id = "gestation";
+    pAsset15.name = "trait_group_gestation";
+    pAsset15.color = "#C44FFF";
+    this.add(pAsset15);
+    SubspeciesTraitGroupAsset pAsset16 = new SubspeciesTraitGroupAsset();
+    pAsset16.id = "eggs";
+    pAsset16.name = "trait_group_eggs";
+    pAsset16.color = "#FFFFFF";
+    this.add(pAsset16);
+    SubspeciesTraitGroupAsset pAsset17 = new SubspeciesTraitGroupAsset();
+    pAsset17.id = "mutations";
+    pAsset17.name = "trait_group_mutations";
+    pAsset17.color = "#64FF4F";
+    this.add(pAsset17);
+    SubspeciesTraitGroupAsset pAsset18 = new SubspeciesTraitGroupAsset();
+    pAsset18.id = "adaptations";
+    pAsset18.name = "trait_group_adaptations";
+    pAsset18.color = "#D8D8D8";
+    this.add(pAsset18);
+    SubspeciesTraitGroupAsset pAsset19 = new SubspeciesTraitGroupAsset();
+    pAsset19.id = "fate";
+    pAsset19.name = "trait_group_fate";
+    pAsset19.color = "#ffd82f";
+    this.add(pAsset19);
+    SubspeciesTraitGroupAsset pAsset20 = new SubspeciesTraitGroupAsset();
+    pAsset20.id = "phenotypes";
+    pAsset20.name = "trait_group_phenotypes";
+    pAsset20.color = "#D8D8D8";
+    this.add(pAsset20);
+    SubspeciesTraitGroupAsset pAsset21 = new SubspeciesTraitGroupAsset();
+    pAsset21.id = "special";
+    pAsset21.name = "trait_group_special";
+    pAsset21.color = "#FF8F44";
+    this.add(pAsset21);
+  }
+}
