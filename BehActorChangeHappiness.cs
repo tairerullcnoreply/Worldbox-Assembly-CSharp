49cf21d806d8ca57823f50b3a1ff6f3e78fd106a
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:04 -0500

    Add BehActorChangeHappiness.cs

---
+++ BehActorChangeHappiness.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehActorChangeHappiness
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class BehActorChangeHappiness : BehaviourActionActor
+{
+  private string _happiness_id;
+
+  public BehActorChangeHappiness(string pID) => this._happiness_id = pID;
+
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.changeHappiness(this._happiness_id);
+    return BehResult.Continue;
+  }
+}
