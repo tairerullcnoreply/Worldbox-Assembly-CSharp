476e86078b98e262b2358cec39e22137dd241085
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:05:43 -0500

    Add BuildingRendererSettings.cs

---
+++ BuildingRendererSettings.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BuildingRendererSettings
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class BuildingRendererSettings
+{
+  private const string material_building = "building";
+  public static bool wobbly_material_enabled = true;
+  public static string cur_default_material = "building";
+}
