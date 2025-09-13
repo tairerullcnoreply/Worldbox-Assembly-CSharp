44623828d1264aa0187e9bd6c73f7419acf18a9b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:20:35 -0500

    Add ReligionTraitGroupLibrary.cs

---
+++ ReligionTraitGroupLibrary.cs
@@ -0,0 +1,64 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ReligionTraitGroupLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class ReligionTraitGroupLibrary : BaseCategoryLibrary<ReligionTraitGroupAsset>
+{
+  public override void init()
+  {
+    base.init();
+    ReligionTraitGroupAsset pAsset1 = new ReligionTraitGroupAsset();
+    pAsset1.id = "harmony";
+    pAsset1.name = "trait_group_harmony";
+    pAsset1.color = "#FFE8AA";
+    this.add(pAsset1);
+    ReligionTraitGroupAsset pAsset2 = new ReligionTraitGroupAsset();
+    pAsset2.id = "creation";
+    pAsset2.name = "trait_group_creation";
+    pAsset2.color = "#A5FF7F";
+    this.add(pAsset2);
+    ReligionTraitGroupAsset pAsset3 = new ReligionTraitGroupAsset();
+    pAsset3.id = "destruction";
+    pAsset3.name = "trait_group_destruction";
+    pAsset3.color = "#FF4949";
+    this.add(pAsset3);
+    ReligionTraitGroupAsset pAsset4 = new ReligionTraitGroupAsset();
+    pAsset4.id = "restoration";
+    pAsset4.name = "trait_group_restoration";
+    pAsset4.color = "#FFE97C";
+    this.add(pAsset4);
+    ReligionTraitGroupAsset pAsset5 = new ReligionTraitGroupAsset();
+    pAsset5.id = "necromancy";
+    pAsset5.name = "trait_group_necromancy";
+    pAsset5.color = "#ACB6C1";
+    this.add(pAsset5);
+    ReligionTraitGroupAsset pAsset6 = new ReligionTraitGroupAsset();
+    pAsset6.id = "protection";
+    pAsset6.name = "trait_group_protection";
+    pAsset6.color = "#56FFFF";
+    this.add(pAsset6);
+    ReligionTraitGroupAsset pAsset7 = new ReligionTraitGroupAsset();
+    pAsset7.id = "the_void";
+    pAsset7.name = "trait_group_the_void";
+    pAsset7.color = "#FF00D5";
+    this.add(pAsset7);
+    ReligionTraitGroupAsset pAsset8 = new ReligionTraitGroupAsset();
+    pAsset8.id = "transformation";
+    pAsset8.name = "trait_group_transformation";
+    pAsset8.color = "#C2C1FF";
+    this.add(pAsset8);
+    ReligionTraitGroupAsset pAsset9 = new ReligionTraitGroupAsset();
+    pAsset9.id = "fate";
+    pAsset9.name = "trait_group_fate";
+    pAsset9.color = "#ffd82f";
+    this.add(pAsset9);
+    ReligionTraitGroupAsset pAsset10 = new ReligionTraitGroupAsset();
+    pAsset10.id = "special";
+    pAsset10.name = "trait_group_special";
+    pAsset10.color = "#FF8F44";
+    this.add(pAsset10);
+  }
+}
