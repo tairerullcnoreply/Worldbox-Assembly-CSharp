1b4698c0e4c4ea838caf0874b3c6d3cf2217d08e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:29:06 -0500

    Add ai/behaviours/BehBoat.cs

---
+++ ai/behaviours/BehBoat.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehBoat
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehBoat : BehaviourActionActor
+{
+  internal Boat boat;
+
+  internal void checkHomeDocks(Actor pActor) => ActorTool.checkHomeDocks(pActor);
+
+  public override void prepare(Actor pActor)
+  {
+    base.prepare(pActor);
+    this.boat = pActor.getSimpleComponent<Boat>();
+  }
+
+  public override BehResult execute(Actor pActor) => BehResult.Continue;
+}
