e6071e083900ae5fa77e0a19ddadc4d4ed11cc97
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:11:22 -0500

    Add GeneratedRoom.cs

---
+++ GeneratedRoom.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GeneratedRoom
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class GeneratedRoom
+{
+  public int id_debug;
+  public List<WorldTile> edges_left;
+  public List<WorldTile> edges_right;
+  public List<WorldTile> edges_up;
+  public List<WorldTile> edges_down;
+}
