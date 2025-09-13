1e2e9b7ff7d01937ef54e6cd48f721e6ab828af6
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:13:50 -0500

    Add Job`1.cs

---
+++ Job`1.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Job`1
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class Job<T>
+{
+  public JobUpdater job_updater;
+  public ObjectContainer<T> container;
+  public string id;
+  public double time_benchmark;
+  public int counter;
+  public int random_tick_skips;
+  public int current_skips;
+}
