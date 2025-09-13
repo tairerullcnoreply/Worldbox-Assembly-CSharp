8b2846d61b63f8bad77e33a2ecd2c857e32fb372
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:24:05 -0500

    Add TesterBehSaveWorldIfEmpty.cs

---
+++ TesterBehSaveWorldIfEmpty.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehSaveWorldIfEmpty
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class TesterBehSaveWorldIfEmpty : BehaviourActionTester
+{
+  private int slot;
+
+  public TesterBehSaveWorldIfEmpty(int pSlot) => this.slot = pSlot;
+
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    SaveManager.setCurrentSlot(this.slot);
+    if (!SaveManager.currentSlotExists())
+      SaveManager.saveWorldToDirectory(SaveManager.currentSavePath);
+    return BehResult.Continue;
+  }
+}
