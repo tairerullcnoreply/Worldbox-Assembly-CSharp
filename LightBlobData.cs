affa09a3c31caa6b45fe133e1635f9b25515cacf
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:14:56 -0500

    Add LightBlobData.cs

---
+++ LightBlobData.cs
@@ -0,0 +1,14 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: LightBlobData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public struct LightBlobData
+{
+  public Vector2 position;
+  public float radius;
+}
