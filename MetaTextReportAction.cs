dd3183392b8e6273c2d2f583de57534136982587
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:16:43 -0500

    Add MetaTextReportAction.cs

---
+++ MetaTextReportAction.cs
@@ -0,0 +1,8 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MetaTextReportAction
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public delegate bool MetaTextReportAction(IMetaObject pMetaObject);
