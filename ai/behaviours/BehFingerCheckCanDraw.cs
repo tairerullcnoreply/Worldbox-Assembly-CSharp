0aa15c232e5404db119fb238467e0c63a8f463b9
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:52 -0500

    Add ai/behaviours/BehFingerCheckCanDraw.cs

---
+++ ai/behaviours/BehFingerCheckCanDraw.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehFingerCheckCanDraw
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehFingerCheckCanDraw : BehFingerDrawAction
+{
+  protected override void setupErrorChecks()
+  {
+    this.check_has_target_tiles = true;
+    this.check_current_tile_in_target_tiles = true;
+    this.check_target_tile_in_target_tiles = false;
+    base.setupErrorChecks();
+  }
+}
