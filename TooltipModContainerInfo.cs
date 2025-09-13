18618f01c5b488f1c7afd7e86e89b6670cdb678c
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:25:19 -0500

    Add TooltipModContainerInfo.cs

---
+++ TooltipModContainerInfo.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TooltipModContainerInfo
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public readonly struct TooltipModContainerInfo(ItemAsset pAsset, int pPluses, string pStringPluses)
+{
+  public readonly ItemAsset asset = pAsset;
+  public readonly int pluses = pPluses;
+  public readonly string string_pluses = pStringPluses;
+}
