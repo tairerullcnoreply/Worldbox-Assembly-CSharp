f28cc4961ac71bf751169ea25e1b35e87d123b0a
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:02:20 -0500

    Add AvatarsCombineDataElement.cs

---
+++ AvatarsCombineDataElement.cs
@@ -0,0 +1,12 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AvatarsCombineDataElement
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public struct AvatarsCombineDataElement(int pOrderIndex, int pTotalAmount)
+{
+  public readonly int order_index = pOrderIndex;
+  public readonly int total_amount = pTotalAmount;
+}
