a808abd8c72e61357106481cdd8737c8497e89cd
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:30 -0500

    Add ai/behaviours/BehFindBuildingWithFood.cs

---
+++ ai/behaviours/BehFindBuildingWithFood.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehFindBuildingWithFood
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehFindBuildingWithFood : BehCityActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    Building storageWithFoodNear = pActor.city.getStorageWithFoodNear(pActor.current_tile);
+    if (storageWithFoodNear == null)
+      return BehResult.Stop;
+    pActor.beh_building_target = storageWithFoodNear;
+    return BehResult.Continue;
+  }
+}
