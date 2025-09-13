b2a0c8593aed5b83bdeaf3222e0054bd355372a9
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:18:36 -0500

    Add PivotPresets.cs

---
+++ PivotPresets.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: PivotPresets
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum PivotPresets
+{
+  TopLeft,
+  TopCenter,
+  TopRight,
+  MiddleLeft,
+  MiddleCenter,
+  MiddleRight,
+  BottomLeft,
+  BottomCenter,
+  BottomRight,
+}
