2b694e65f8c58cd019e5c979db4d1acefa0ae3cd
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:10:28 -0500

    Add EquipmentGroupElement.cs

---
+++ EquipmentGroupElement.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EquipmentGroupElement
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class EquipmentGroupElement : 
+  AugmentationCategory<EquipmentAsset, EquipmentButton, EquipmentEditorButton>
+{
+  protected override bool isUnlocked(EquipmentButton pButton)
+  {
+    return pButton.getElementAsset().isAvailable();
+  }
+}
