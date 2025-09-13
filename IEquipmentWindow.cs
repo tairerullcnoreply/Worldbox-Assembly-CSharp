b62717f16fee641ad8a59aab456981fe7a9a9e9c
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:12:52 -0500

    Add IEquipmentWindow.cs

---
+++ IEquipmentWindow.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: IEquipmentWindow
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public interface IEquipmentWindow : IAugmentationsWindow<IEquipmentEditor>
+{
+  void reloadEquipment()
+  {
+    this.GetComponentInChildren<UnitEquipmentContainer>().reloadEquipment(false);
+  }
+
+  void checkEquipmentTabIcon();
+}
