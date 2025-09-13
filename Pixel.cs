b01afcd6037b4de03c0cab13eba110f602caa4c1
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:18:37 -0500

    Add Pixel.cs

---
+++ Pixel.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Pixel
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public struct Pixel(int pX, int pY, Color32 pColor)
+{
+  public readonly int x = pX;
+  public readonly int y = pY;
+  public readonly Color32 color = pColor;
+}
