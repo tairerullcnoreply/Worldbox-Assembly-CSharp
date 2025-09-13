4afcb6cf169c16d9ffc2ac2a9e24022633694ca1
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:31:27 -0500

    Add ai/behaviours/BehRepeatTaskChance.cs

---
+++ ai/behaviours/BehRepeatTaskChance.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehRepeatTaskChance
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehRepeatTaskChance : BehaviourActionActor
+{
+  private float chance;
+
+  public BehRepeatTaskChance(float pChance = 0.5f) => this.chance = pChance;
+
+  public override BehResult execute(Actor pActor)
+  {
+    return Randy.randomChance(this.chance) ? BehResult.RestartTask : BehResult.Continue;
+  }
+}
