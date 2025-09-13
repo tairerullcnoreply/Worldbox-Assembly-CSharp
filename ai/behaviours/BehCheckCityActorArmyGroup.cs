656bd0ecb9214e2a85ff977d20eaf4e4d88dad83
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:29:27 -0500

    Add ai/behaviours/BehCheckCityActorArmyGroup.cs

---
+++ ai/behaviours/BehCheckCityActorArmyGroup.cs
@@ -0,0 +1,27 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehCheckCityActorArmyGroup
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehCheckCityActorArmyGroup : BehCitizenActionCity
+{
+  public override BehResult execute(Actor pActor)
+  {
+    if (!pActor.isKingdomCiv())
+    {
+      pActor.stopBeingWarrior();
+      return BehResult.Stop;
+    }
+    if (pActor.city.hasArmy())
+    {
+      Army army = pActor.city.army;
+      if (army.isGroupInCityAndHaveLeader())
+        pActor.setArmy(army);
+    }
+    return BehResult.Continue;
+  }
+}
