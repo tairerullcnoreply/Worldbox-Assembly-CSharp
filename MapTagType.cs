06660da23b3e30f2298478f8c0d2e1611adf3dce
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:16:08 -0500

    Add MapTagType.cs

---
+++ MapTagType.cs
@@ -0,0 +1,30 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MapTagType
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum MapTagType
+{
+  General,
+  Realistic,
+  Earth,
+  Fantasy,
+  Populated,
+  Unpopulated,
+  Country,
+  Continent,
+  Natural,
+  Unnatural,
+  Civilizations,
+  Battle,
+  WorldWar,
+  SciFi,
+  ArtToMap,
+  PerfectBalance,
+  Disasters,
+  Magic,
+  Anime,
+  Other,
+}
