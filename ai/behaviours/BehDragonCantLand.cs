b48aab3509e0984dc515c6b49471208f0296fd5b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:05 -0500

    Add ai/behaviours/BehDragonCantLand.cs

---
+++ ai/behaviours/BehDragonCantLand.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehDragonCantLand
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehDragonCantLand : BehaviourActionActor
+{
+  private string task_id;
+
+  public BehDragonCantLand(string pNextAction) => this.task_id = pNextAction;
+
+  public override BehResult execute(Actor pActor)
+  {
+    return !Dragon.canLand(pActor) ? this.forceTask(pActor, this.task_id) : BehResult.Continue;
+  }
+}
