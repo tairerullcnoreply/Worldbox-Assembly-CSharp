78cc2b50e825203055aa9eff7144783141849d4c
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:21:18 -0500

    Add SaveWorldButton.cs

---
+++ SaveWorldButton.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SaveWorldButton
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+#nullable disable
+public class SaveWorldButton : MonoBehaviour
+{
+  private void Start()
+  {
+    Button button;
+    if (!((Component) this).TryGetComponent<Button>(ref button))
+      return;
+    // ISSUE: method pointer
+    ((UnityEvent) button.onClick).AddListener(new UnityAction((object) this, __methodptr(saveWorld)));
+  }
+
+  private void saveWorld() => ScrollWindow.showWindow("save_world_confirm");
+}
