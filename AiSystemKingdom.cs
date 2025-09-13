ab3bcdf85dd6e2e9d0b590730bcc08d0a5921d0b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:01:14 -0500

    Add AiSystemKingdom.cs

---
+++ AiSystemKingdom.cs
@@ -0,0 +1,11 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AiSystemKingdom
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class AiSystemKingdom(Kingdom pObject) : 
+  AiSystem<Kingdom, KingdomJob, BehaviourTaskKingdom, BehaviourActionKingdom, BehaviourKingdomCondition>(pObject)
+{
+}
