644ea0b9f76da78ae743d3e57e4656f3938d7172
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:48 -0500

    Add TesterBehDestroySimObjects.cs

---
+++ TesterBehDestroySimObjects.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehDestroySimObjects
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class TesterBehDestroySimObjects : BehaviourActionTester
+{
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    foreach (BaseSimObject unit in (SimSystemManager<Actor, ActorData>) BehaviourActionBase<AutoTesterBot>.world.units)
+      unit.getHitFullHealth(AttackType.Divine);
+    foreach (BaseSimObject building in (SimSystemManager<Building, BuildingData>) BehaviourActionBase<AutoTesterBot>.world.buildings)
+      building.getHitFullHealth(AttackType.Divine);
+    return base.execute(pObject);
+  }
+}
