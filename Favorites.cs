0ba2573676dd0fd4d04085f8ea3b5312fe8b6bde
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:10:51 -0500

    Add Favorites.cs

---
+++ Favorites.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Favorites
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using RSG;
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+[Serializable]
+public class Favorites
+{
+  public static Dictionary<string, bool> favorites = new Dictionary<string, bool>();
+  public static Promise promise;
+}
