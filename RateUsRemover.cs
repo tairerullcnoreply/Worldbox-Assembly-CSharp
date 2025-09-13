936464bbec029bbe86e59707df77d87281104ab2
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:20:11 -0500

    Add RateUsRemover.cs

---
+++ RateUsRemover.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: RateUsRemover
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class RateUsRemover : MonoBehaviour
+{
+  public void clickedRateUs()
+  {
+    PlayerConfig.instance.data.lastRateID = 12;
+    ((Component) this).gameObject.SetActive(false);
+    PlayerConfig.saveData();
+  }
+}
