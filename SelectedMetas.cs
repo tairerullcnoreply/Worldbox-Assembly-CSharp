e748e0f48ff9a2ac8e21b5ab7e80a7eeec21bcf8
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:21:31 -0500

    Add SelectedMetas.cs

---
+++ SelectedMetas.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SelectedMetas
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public static class SelectedMetas
+{
+  public static Alliance selected_alliance;
+  public static Army selected_army;
+  public static City selected_city;
+  public static Clan selected_clan;
+  public static Culture selected_culture;
+  public static Family selected_family;
+  public static Item selected_item;
+  public static Kingdom selected_kingdom;
+  public static Language selected_language;
+  public static Plot selected_plot;
+  public static Religion selected_religion;
+  public static Subspecies selected_subspecies;
+  public static War selected_war;
+}
