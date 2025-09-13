7159ec4f3f401e831fe6c0324ee07fd9449cd5d6
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:15:12 -0500

    Add ListWindowStatistics.cs

---
+++ ListWindowStatistics.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ListWindowStatistics
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class ListWindowStatistics : StatisticsRows
+{
+  public MetaType meta_type;
+
+  protected override void init()
+  {
+    foreach (StatisticsAsset pAsset in AssetManager.statistics_library.list)
+    {
+      if (!pAsset.list_window_meta_type.isNone() && this.meta_type == pAsset.list_window_meta_type)
+        this.addStatRow(pAsset);
+    }
+  }
+}
