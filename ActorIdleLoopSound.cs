c3a2cebe4043ec77b5ead038b41b7c44d8a04607
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:00:37 -0500

    Add ActorIdleLoopSound.cs

---
+++ ActorIdleLoopSound.cs
@@ -0,0 +1,28 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ActorIdleLoopSound
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using FMOD.Studio;
+
+#nullable disable
+public class ActorIdleLoopSound
+{
+  internal EventInstance fmod_instance;
+  private Actor _actor;
+
+  public ActorIdleLoopSound(ActorAsset pAsset, Actor pActor)
+  {
+  }
+
+  public void stop() => this.stopLoopCallback(this._actor);
+
+  internal void stopLoopCallback(Actor pActor)
+  {
+    if (!((EventInstance) ref this.fmod_instance).isValid())
+      return;
+    ((EventInstance) ref this.fmod_instance).stop((STOP_MODE) 0);
+    ((EventInstance) ref this.fmod_instance).release();
+  }
+}
