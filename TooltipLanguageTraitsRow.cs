3741aa827d6cd42465eb933011680599f5935b9e
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:25:17 -0500

    Add TooltipLanguageTraitsRow.cs

---
+++ TooltipLanguageTraitsRow.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TooltipLanguageTraitsRow
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class TooltipLanguageTraitsRow : TooltipTraitsRow<LanguageTrait>
+{
+  protected override IReadOnlyCollection<LanguageTrait> traits_hashset
+  {
+    get => this.tooltip_data.language.getTraits();
+  }
+}
