9365a9b1fef7799bd74998c60a38baed65ae23be
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:22 -0500

    Add TabCenterer.cs

---
+++ TabCenterer.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TabCenterer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class TabCenterer : MonoBehaviour
+{
+  [SerializeField]
+  private PowersTab _powers_tab;
+  public const bool ENABLE_CENTERING = false;
+
+  private void Update() => this.centerTab();
+
+  public void centerTab()
+  {
+  }
+}
