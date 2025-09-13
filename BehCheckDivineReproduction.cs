772373c1d3b7b284a1ded5e0cf167ea041767944
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:33 -0500

    Add BehCheckDivineReproduction.cs

---
+++ BehCheckDivineReproduction.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehCheckDivineReproduction
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class BehCheckDivineReproduction : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    if (pActor.hasTrait("miracle_bearer"))
+      return BehResult.Stop;
+    BabyMaker.startMiracleBirth(pActor);
+    return BehResult.Continue;
+  }
+}
