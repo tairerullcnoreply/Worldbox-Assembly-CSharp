adb1b42e5de74890a36313fb08aa2e0de8efa3c9
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:38 -0500

    Add TestEnum.cs

---
+++ TestEnum.cs
@@ -0,0 +1,31 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TestEnum
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+internal enum TestEnum
+{
+  Val0,
+  Val1,
+  Val2,
+  Val3,
+  Val4,
+  Val5,
+  Val6,
+  Val7,
+  Val8,
+  Val9,
+  Val10,
+  Val11,
+  Val12,
+  Val13,
+  Val14,
+  Val15,
+  Val16,
+  Val17,
+  Val18,
+  Val19,
+  Val20,
+}
