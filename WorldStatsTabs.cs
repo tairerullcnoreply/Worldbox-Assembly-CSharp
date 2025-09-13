1b7866ea4ad04810d2bbacdb1473c78d176bc5cd
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:28:13 -0500

    Add WorldStatsTabs.cs

---
+++ WorldStatsTabs.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WorldStatsTabs
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+[Flags]
+public enum WorldStatsTabs
+{
+  Everything = 0,
+  General = 1,
+  Noosphere = 2,
+  Deaths = 4,
+  Biomes = 8,
+}
