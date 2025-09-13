f304499761d57f2be7aea25c51af04de5265c0b9
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:14:25 -0500

    Add KingdomWarsContainer.cs

---
+++ KingdomWarsContainer.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: KingdomWarsContainer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections;
+
+#nullable disable
+public class KingdomWarsContainer : KingdomDiplomacyContainer<WarBanner, War, WarData>
+{
+  protected override IEnumerator showContent()
+  {
+    // ISSUE: unable to decompile the method.
+  }
+}
