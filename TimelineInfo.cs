a5fbdf47135076f17db352f37908f3d42da8c124
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:24:56 -0500

    Add TimelineInfo.cs

---
+++ TimelineInfo.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TimelineInfo
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using FMOD;
+using System.Runtime.InteropServices;
+
+#nullable disable
+[StructLayout(LayoutKind.Sequential)]
+internal class TimelineInfo
+{
+  public int currentMusicBar;
+  public StringWrapper lastMarker;
+}
