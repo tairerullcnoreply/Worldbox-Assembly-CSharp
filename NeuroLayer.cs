5cee53e25d8375ee9f04703b0c564aa66bc98175
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:17:45 -0500

    Add NeuroLayer.cs

---
+++ NeuroLayer.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: NeuroLayer
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum NeuroLayer
+{
+  Layer_0_Minimal,
+  Layer_1_Low,
+  Layer_2_Moderate,
+  Layer_3_High,
+  Layer_4_Critical,
+}
