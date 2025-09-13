ba150832b9c408d31cf5e74b2b10962bd9608273
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:12:43 -0500

    Add IComponentList.cs

---
+++ IComponentList.cs
@@ -0,0 +1,34 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: IComponentList
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#nullable disable
+public interface IComponentList
+{
+  ListPool<NanoObject> getElements();
+
+  void setShowAll();
+
+  void setShowFavoritesOnly();
+
+  void setShowDeadOnly();
+
+  void setShowAliveOnly();
+
+  void setDefault();
+
+  void init(
+    GameObject pNoItems,
+    SortingTab pSortingTab,
+    GameObject pListElementPrefab,
+    Transform pListTransform,
+    ScrollRect pScrollRect,
+    Text pTitleCounter,
+    Text pFavoritesCounter,
+    Text pDeadCounter);
+}
