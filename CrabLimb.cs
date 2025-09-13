3cc5d756adad34d4fc2e9091e92abd89bc9883e5
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:08:10 -0500

    Add CrabLimb.cs

---
+++ CrabLimb.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CrabLimb
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum CrabLimb
+{
+  ArmRight,
+  ArmLeft,
+  LegRight1,
+  LegRight2,
+  LegRight3,
+  LegLeft1,
+  LegLeft2,
+  LegLeft3,
+  MainBody,
+}
