45fcd69001a6d7618033149c0d8a2ade860c8dba
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:31:52 -0500

    Add ai/behaviours/BehUFOExplore.cs

---
+++ ai/behaviours/BehUFOExplore.cs
@@ -0,0 +1,17 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehUFOExplore
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehUFOExplore : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.data.set("exploringTicks", Randy.randomInt(3, 7));
+    return BehResult.Continue;
+  }
+}
