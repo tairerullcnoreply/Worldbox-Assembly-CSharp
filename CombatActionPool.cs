3fa97827d1a59dc0eb1e84ea9e47deb8ba93c14a
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:07:45 -0500

    Add CombatActionPool.cs

---
+++ CombatActionPool.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CombatActionPool
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum CombatActionPool
+{
+  BEFORE_HIT_DEFLECT,
+  BEFORE_HIT_BLOCK,
+  BEFORE_HIT,
+  BEFORE_ATTACK_MELEE,
+  BEFORE_ATTACK_RANGE,
+}
