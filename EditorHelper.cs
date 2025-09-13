524b31ff5605a79fe79a58a6db0e534b6cfc7781
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:10:04 -0500

    Add EditorHelper.cs

---
+++ EditorHelper.cs
@@ -0,0 +1,32 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EditorHelper
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+public static class EditorHelper
+{
+  public static bool HasArgument(string pName)
+  {
+    foreach (string commandLineArg in Environment.GetCommandLineArgs())
+    {
+      if (commandLineArg.Contains(pName))
+        return true;
+    }
+    return false;
+  }
+
+  public static string GetArgument(string pName)
+  {
+    string[] commandLineArgs = Environment.GetCommandLineArgs();
+    for (int index = 0; index < commandLineArgs.Length; ++index)
+    {
+      if (commandLineArgs[index].Contains(pName))
+        return commandLineArgs[index + 1];
+    }
+    return (string) null;
+  }
+}
