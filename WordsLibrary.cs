da8a7e4751492166dfba514c2b4ba53260ad05b5
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:27:19 -0500

    Add WordsLibrary.cs

---
+++ WordsLibrary.cs
@@ -0,0 +1,115 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WordsLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class WordsLibrary : AssetLibrary<WordAsset>
+{
+  public override void init()
+  {
+    WordAsset pAsset1 = new WordAsset();
+    pAsset1.id = "pron_subj_i";
+    this.add(pAsset1);
+    WordAsset pAsset2 = new WordAsset();
+    pAsset2.id = "pron_subj_we";
+    this.add(pAsset2);
+    WordAsset pAsset3 = new WordAsset();
+    pAsset3.id = "pron_subj_you";
+    this.add(pAsset3);
+    WordAsset pAsset4 = new WordAsset();
+    pAsset4.id = "pron_subj_it";
+    this.add(pAsset4);
+    WordAsset pAsset5 = new WordAsset();
+    pAsset5.id = "pron_subj_they";
+    this.add(pAsset5);
+    WordAsset pAsset6 = new WordAsset();
+    pAsset6.id = "pron_obj_me";
+    this.add(pAsset6);
+    WordAsset pAsset7 = new WordAsset();
+    pAsset7.id = "pron_obj_us";
+    this.add(pAsset7);
+    WordAsset pAsset8 = new WordAsset();
+    pAsset8.id = "pron_obj_you";
+    this.add(pAsset8);
+    WordAsset pAsset9 = new WordAsset();
+    pAsset9.id = "pron_obj_it";
+    this.add(pAsset9);
+    WordAsset pAsset10 = new WordAsset();
+    pAsset10.id = "pron_obj_them";
+    this.add(pAsset10);
+    WordAsset pAsset11 = new WordAsset();
+    pAsset11.id = "pron_poss_my";
+    this.add(pAsset11);
+    WordAsset pAsset12 = new WordAsset();
+    pAsset12.id = "pron_poss_our";
+    this.add(pAsset12);
+    WordAsset pAsset13 = new WordAsset();
+    pAsset13.id = "pron_poss_your";
+    this.add(pAsset13);
+    WordAsset pAsset14 = new WordAsset();
+    pAsset14.id = "pron_poss_its";
+    this.add(pAsset14);
+    WordAsset pAsset15 = new WordAsset();
+    pAsset15.id = "pron_poss_their";
+    this.add(pAsset15);
+    WordAsset pAsset16 = new WordAsset();
+    pAsset16.id = "pron_poss_mine";
+    this.add(pAsset16);
+    WordAsset pAsset17 = new WordAsset();
+    pAsset17.id = "pron_poss_ours";
+    this.add(pAsset17);
+    WordAsset pAsset18 = new WordAsset();
+    pAsset18.id = "pron_poss_yours";
+    this.add(pAsset18);
+    WordAsset pAsset19 = new WordAsset();
+    pAsset19.id = "pron_poss_theirs";
+    this.add(pAsset19);
+    WordAsset pAsset20 = new WordAsset();
+    pAsset20.id = "concept_love";
+    this.add(pAsset20);
+    WordAsset pAsset21 = new WordAsset();
+    pAsset21.id = "concept_death";
+    this.add(pAsset21);
+    WordAsset pAsset22 = new WordAsset();
+    pAsset22.id = "concept_nature";
+    this.add(pAsset22);
+    WordAsset pAsset23 = new WordAsset();
+    pAsset23.id = "action_run";
+    this.add(pAsset23);
+    WordAsset pAsset24 = new WordAsset();
+    pAsset24.id = "action_walk";
+    this.add(pAsset24);
+    WordAsset pAsset25 = new WordAsset();
+    pAsset25.id = "action_fight";
+    this.add(pAsset25);
+    WordAsset pAsset26 = new WordAsset();
+    pAsset26.id = "object_sword";
+    this.add(pAsset26);
+    WordAsset pAsset27 = new WordAsset();
+    pAsset27.id = "object_shield";
+    this.add(pAsset27);
+    WordAsset pAsset28 = new WordAsset();
+    pAsset28.id = "object_bow";
+    this.add(pAsset28);
+    WordAsset pAsset29 = new WordAsset();
+    pAsset29.id = "place_forest";
+    this.add(pAsset29);
+    WordAsset pAsset30 = new WordAsset();
+    pAsset30.id = "place_mountain";
+    this.add(pAsset30);
+    WordAsset pAsset31 = new WordAsset();
+    pAsset31.id = "place_hill";
+    this.add(pAsset31);
+    WordAsset pAsset32 = new WordAsset();
+    pAsset32.id = "creature_king";
+    this.add(pAsset32);
+    WordAsset pAsset33 = new WordAsset();
+    pAsset33.id = "creature_queen";
+    this.add(pAsset33);
+    WordAsset pAsset34 = new WordAsset();
+    pAsset34.id = "creature_prince";
+    this.add(pAsset34);
+  }
+}
