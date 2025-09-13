a1c46a014d0c34e6a2dae5841a5c89b93f9f2c65
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:02:43 -0500

    Add BaseLibraryWithUnlockables`1.cs

---
+++ BaseLibraryWithUnlockables`1.cs
@@ -0,0 +1,30 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BaseLibraryWithUnlockables`1
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+[Serializable]
+public class BaseLibraryWithUnlockables<T> : AssetLibrary<T>, ILibraryWithUnlockables where T : BaseUnlockableAsset
+{
+  [JsonIgnore]
+  public IEnumerable<BaseUnlockableAsset> elements_list
+  {
+    get => (IEnumerable<BaseUnlockableAsset>) this.list;
+  }
+
+  public override void editorDiagnosticLocales()
+  {
+    base.editorDiagnosticLocales();
+    foreach (T obj in this.list)
+    {
+      BaseUnlockableAsset pAsset = (BaseUnlockableAsset) obj;
+      this.checkLocale((Asset) pAsset, pAsset.getLocaleID());
+    }
+  }
+}
