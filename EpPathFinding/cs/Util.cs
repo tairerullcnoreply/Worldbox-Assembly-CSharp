eadfe62a3b1c83cd3b9645a4afd1c8621230d854
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:32:54 -0500

    Add EpPathFinding/cs/Util.cs

---
+++ EpPathFinding/cs/Util.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EpPathFinding.cs.Util
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace EpPathFinding.cs;
+
+public class Util
+{
+  public static DiagonalMovement GetDiagonalMovement(bool iCrossCorners, bool iCrossAdjacentPoint)
+  {
+    if (iCrossCorners & iCrossAdjacentPoint)
+      return DiagonalMovement.Always;
+    return iCrossCorners ? DiagonalMovement.IfAtLeastOneWalkable : DiagonalMovement.OnlyWhenNoObstacles;
+  }
+}
