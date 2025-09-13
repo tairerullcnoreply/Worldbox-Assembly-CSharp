8b814f8260701c3fcd854ee048b9b480028a967b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:16:32 -0500

    Add MetaObjectCounter`2.cs

---
+++ MetaObjectCounter`2.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MetaObjectCounter`2
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class MetaObjectCounter<TObject, TData>
+  where TObject : MetaObject<TData>
+  where TData : MetaObjectData
+{
+  public TObject meta_object;
+  public int amount;
+
+  public MetaObjectCounter(TObject pMetaObject) => this.meta_object = pMetaObject;
+}
