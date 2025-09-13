33fd248163c1dc378743ba3fc6b1f9341a5ba17c
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:05:21 -0500

    Add BuildingAnimationData.cs

---
+++ BuildingAnimationData.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BuildingAnimationData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class BuildingAnimationData
+{
+  public bool animated;
+  public ListPool<Sprite> list_spawn;
+  public ListPool<Sprite> list_main;
+  public ListPool<Sprite> list_main_disabled;
+  public ListPool<Sprite> list_ruins;
+  public ListPool<Sprite> list_special;
+  public Sprite[] main;
+  public Sprite[] main_disabled;
+  public Sprite[] spawn;
+  public Sprite[] ruins;
+  public Sprite[] special;
+}
