0776e5f5f4aa6a0fbc0a8c050360261a0c1e7f85
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:10:05 -0500

    Add EffectAction.cs

---
+++ EffectAction.cs
@@ -0,0 +1,14 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EffectAction
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public delegate BaseEffect EffectAction(
+  BaseEffect pEffect,
+  WorldTile pTile = null,
+  string pParam1 = null,
+  string pParam2 = null,
+  float pFloatParam1 = 0.0f,
+  Actor pActor = null);
