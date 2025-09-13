9dcca3ce59aaeb761b8ad87b5bd4faae9ce9b8fc
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:21:28 -0500

    Add SelectedElementBase`1.cs

---
+++ SelectedElementBase`1.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SelectedElementBase`1
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class SelectedElementBase<TComponent> : MonoBehaviour where TComponent : Component
+{
+  [SerializeField]
+  protected Transform _grid;
+  protected ObjectPoolGenericMono<TComponent> _pool;
+
+  protected void clear() => this._pool?.clear();
+
+  protected virtual void refresh(NanoObject pNano)
+  {
+  }
+
+  private void OnDisable() => this.clear();
+}
