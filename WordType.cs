24ed3a82eac895077a9f731d5936b20653d336c7
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:27:20 -0500

    Add WordType.cs

---
+++ WordType.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WordType
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public enum WordType
+{
+  Name,
+  Place,
+  Concept,
+  Action,
+  Object,
+  Creature,
+  Pronoun,
+  None,
+}
