b0db7f3e1694a75eea794f9c5cb7c0a315439453
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:28:14 -0500

    Add WorldStatus.cs

---
+++ WorldStatus.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WorldStatus
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class WorldStatus : MonoBehaviour
+{
+  public static int currentSlot;
+
+  public void setCurrentSlot(int pSlotId) => WorldStatus.currentSlot = pSlotId;
+}
