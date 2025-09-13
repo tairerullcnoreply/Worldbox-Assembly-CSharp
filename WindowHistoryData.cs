91c10d61d9f7979fe763c74a1f1ef99b7ac29f2f
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:27:08 -0500

    Add WindowHistoryData.cs

---
+++ WindowHistoryData.cs
@@ -0,0 +1,26 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WindowHistoryData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public struct WindowHistoryData
+{
+  public int index;
+  public ScrollWindow window;
+  public Kingdom kingdom;
+  public Culture culture;
+  public Actor unit;
+  public City city;
+  public Clan clan;
+  public Plot plot;
+  public War war;
+  public Alliance alliance;
+  public Language language;
+  public Subspecies subspecies;
+  public Religion religion;
+  public Family family;
+  public Army army;
+  public Item item;
+}
