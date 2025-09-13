901cd6e187a59d59eecb639d030958befa59fdf6
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:26:16 -0500

    Add UnitProfession.cs

---
+++ UnitProfession.cs
@@ -0,0 +1,18 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: UnitProfession
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+public enum UnitProfession
+{
+  Nothing,
+  [Obsolete("Baby not a city job anymore", true)] Baby,
+  Unit,
+  King,
+  Leader,
+  Warrior,
+}
