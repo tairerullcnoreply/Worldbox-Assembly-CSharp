4f214859171268365d8ace239b2cd1a83d6627f8
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:28:42 -0500

    Add ai/behaviours/BehActorCheckBool.cs

---
+++ ai/behaviours/BehActorCheckBool.cs
@@ -0,0 +1,30 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehActorCheckBool
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehActorCheckBool : BehaviourActionActor
+{
+  private string actionIfBool;
+  private string boolCheck;
+
+  public BehActorCheckBool(string pBool, string pActionIfHit)
+  {
+    this.actionIfBool = pActionIfHit;
+    this.boolCheck = pBool;
+  }
+
+  public override BehResult execute(Actor pActor)
+  {
+    bool pResult;
+    pActor.data.get(this.boolCheck, out pResult);
+    if (!pResult)
+      return BehResult.Continue;
+    pActor.data.removeBool(this.boolCheck);
+    return this.forceTask(pActor, this.actionIfBool);
+  }
+}
