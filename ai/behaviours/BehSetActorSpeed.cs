ab804807eb1f268a9b73168784e1732bdca54108
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:31:35 -0500

    Add ai/behaviours/BehSetActorSpeed.cs

---
+++ ai/behaviours/BehSetActorSpeed.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehSetActorSpeed
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehSetActorSpeed : BehaviourActionActor
+{
+  private float speed;
+
+  public BehSetActorSpeed(float pSpeed = 0.0f) => this.speed = pSpeed;
+
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.stats["speed"] = this.speed;
+    return BehResult.Continue;
+  }
+}
