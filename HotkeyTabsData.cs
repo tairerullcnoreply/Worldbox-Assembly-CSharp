5c717f49fa4fdc9acbe2a715b8bb0591bebc8a60
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:12:33 -0500

    Add HotkeyTabsData.cs

---
+++ HotkeyTabsData.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: HotkeyTabsData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+
+#nullable disable
+[Serializable]
+public class HotkeyTabsData
+{
+  public string hotkey_data_1;
+  public string hotkey_data_2;
+  public string hotkey_data_3;
+  public string hotkey_data_4;
+  public string hotkey_data_5;
+  public string hotkey_data_6;
+  public string hotkey_data_7;
+  public string hotkey_data_8;
+  public string hotkey_data_9;
+  public string hotkey_data_0;
+}
