1011e3d34a9fe7bc88553811d6d55d801a7d2e38
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:28:44 -0500

    Add ai/behaviours/BehActorSetFlip.cs

---
+++ ai/behaviours/BehActorSetFlip.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehActorSetFlip
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehActorSetFlip : BehaviourActionActor
+{
+  private bool flip;
+
+  public BehActorSetFlip(bool pFlip) => this.flip = pFlip;
+
+  public override BehResult execute(Actor pActor)
+  {
+    pActor.setFlip(this.flip);
+    return BehResult.Continue;
+  }
+}
