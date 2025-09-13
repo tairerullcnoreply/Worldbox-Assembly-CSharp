8d78b0f901b71b5a160255b4ce33a487afe7376d
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:12:40 -0500

    Add IBaseMetaBanners.cs

---
+++ IBaseMetaBanners.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: IBaseMetaBanners
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+using UnityEngine;
+
+#nullable disable
+public interface IBaseMetaBanners
+{
+  void metaBannerShow(MetaBannerElement pAsset);
+
+  void metaBannerHide(MetaBannerElement pAsset);
+
+  IReadOnlyCollection<MetaBannerElement> getBanners();
+
+  void enableClickAnimation()
+  {
+    foreach (MetaBannerElement banner in (IEnumerable<MetaBannerElement>) this.getBanners())
+      ((Component) banner.banner).GetComponent<TipButton>().showOnClick = true;
+  }
+}
