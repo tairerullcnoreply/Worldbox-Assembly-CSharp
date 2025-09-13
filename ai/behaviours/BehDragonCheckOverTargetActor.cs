24b3121904e2f7769472001804390f520e4a1c46
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:30:08 -0500

    Add ai/behaviours/BehDragonCheckOverTargetActor.cs

---
+++ ai/behaviours/BehDragonCheckOverTargetActor.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.BehDragonCheckOverTargetActor
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours;
+
+public class BehDragonCheckOverTargetActor : BehDragon
+{
+  public override BehResult execute(Actor pActor)
+  {
+    return WorldLawLibrary.world_law_peaceful_monsters.isEnabled() || this.dragon.aggroTargets.Count == 0 || !Dragon.canLand(pActor) || !this.dragon.targetsWithinLandAttackRange() ? BehResult.Continue : this.forceTask(pActor, "dragon_land_attack");
+  }
+}
