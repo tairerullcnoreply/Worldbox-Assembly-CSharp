f336301e9c12a28b7654257bce76ae5419fce684
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:07:40 -0500

    Add CombatActionActor.cs

---
+++ CombatActionActor.cs
@@ -0,0 +1,12 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CombatActionActor
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public delegate bool CombatActionActor(
+  Actor pActor,
+  AttackData pData,
+  float pTargetX = 0.0f,
+  float pTargetY = 0.0f);
