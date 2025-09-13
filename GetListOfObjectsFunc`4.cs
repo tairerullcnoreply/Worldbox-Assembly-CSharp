863a671017be0431e2cf01cb4b1c80d1d1c83436
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:11:37 -0500

    Add GetListOfObjectsFunc`4.cs

---
+++ GetListOfObjectsFunc`4.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GetListOfObjectsFunc`4
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public delegate IEnumerable<TMetaObject> GetListOfObjectsFunc<TListElement, TMetaObject, TData, TComponent>(
+  TComponent pComponent)
+  where TListElement : WindowListElementBase<TMetaObject, TData>
+  where TMetaObject : CoreSystemObject<TData>
+  where TData : BaseSystemData
+  where TComponent : ComponentListBase<TListElement, TMetaObject, TData, TComponent>;
