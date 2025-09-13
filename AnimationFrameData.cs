c8a57d2a7807b71177449c6d2aac77b1bf198aa0
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:01:32 -0500

    Add AnimationFrameData.cs

---
+++ AnimationFrameData.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: AnimationFrameData
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+public class AnimationFrameData
+{
+  internal string id;
+  internal string sheet_path;
+  internal Vector2 pos_head;
+  internal Vector2 pos_head_new;
+  internal Vector2 pos_item;
+  internal Vector2 size_unit;
+  public bool show_head;
+  public bool show_item;
+}
