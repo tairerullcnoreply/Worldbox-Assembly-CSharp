9cf74299a8431d4f078280819dfd032e852a50a4
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:03:57 -0500

    Add BehFindRandomTileAroundHouse.cs

---
+++ BehFindRandomTileAroundHouse.cs
@@ -0,0 +1,24 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BehFindRandomTileAroundHouse
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class BehFindRandomTileAroundHouse : BehaviourActionActor
+{
+  public override BehResult execute(Actor pActor)
+  {
+    Building homeBuilding = pActor.getHomeBuilding();
+    if (homeBuilding == null || !homeBuilding.current_tile.isSameIsland(pActor.current_tile))
+      return BehResult.Stop;
+    MapRegion mapRegion = homeBuilding.current_tile.region;
+    if (Randy.randomChance(0.2f) && mapRegion.neighbours.Count > 0)
+      mapRegion = mapRegion.neighbours.GetRandom<MapRegion>();
+    WorldTile random = mapRegion.tiles.GetRandom<WorldTile>();
+    pActor.beh_tile_target = random;
+    return BehResult.Continue;
+  }
+}
