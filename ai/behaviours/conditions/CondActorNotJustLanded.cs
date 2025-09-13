239f08912b4a37e75e2f93ca6607c7afed509119
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:32:20 -0500

    Add ai/behaviours/conditions/CondActorNotJustLanded.cs

---
+++ ai/behaviours/conditions/CondActorNotJustLanded.cs
@@ -0,0 +1,23 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.conditions.CondActorNotJustLanded
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+
+#nullable disable
+namespace ai.behaviours.conditions;
+
+public class CondActorNotJustLanded : BehaviourActorCondition
+{
+  public override bool check(Actor pActor)
+  {
+    int pResult;
+    pActor.data.get("justLanded", out pResult);
+    int num1 = pResult <= 0 ? 1 : 0;
+    int num2 = pResult - 1;
+    pActor.data.set("justLanded", Mathf.Max(num2, 0));
+    return num1 != 0;
+  }
+}
