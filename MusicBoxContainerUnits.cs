9ee83c883d2d8e890cdb7c0e597ae06dbfc4a8d5
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:17:16 -0500

    Add MusicBoxContainerUnits.cs

---
+++ MusicBoxContainerUnits.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MusicBoxContainerUnits
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class MusicBoxContainerUnits
+{
+  public MusicAsset asset;
+  public int units;
+  public bool enabled;
+
+  public void clear()
+  {
+    this.units = 0;
+    this.enabled = false;
+  }
+}
