70f5252b7a0d3304b5ed8823a069ba6ceac7f864
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:23:51 -0500

    Add TesterBehFindTileForCity.cs

---
+++ TesterBehFindTileForCity.cs
@@ -0,0 +1,26 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TesterBehFindTileForCity
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using ai.behaviours;
+
+#nullable disable
+public class TesterBehFindTileForCity : BehaviourActionTester
+{
+  public override BehResult execute(AutoTesterBot pObject)
+  {
+    TileZone random = BehaviourActionBase<AutoTesterBot>.world.zone_calculator.zones.GetRandom<TileZone>();
+    for (int index = 0; index < 100; ++index)
+    {
+      if (random.isGoodForNewCity())
+      {
+        pObject.beh_tile_target = random.centerTile;
+        return BehResult.Continue;
+      }
+      random = BehaviourActionBase<AutoTesterBot>.world.zone_calculator.zones.GetRandom<TileZone>();
+    }
+    return base.execute(pObject);
+  }
+}
