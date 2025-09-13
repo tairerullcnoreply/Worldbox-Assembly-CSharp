8e0b7788f1a6dbd165e312e027cc555359fe7170
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:09:29 -0500

    Add DictionaryExtensions.cs

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DictionaryExtensions
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+public static class DictionaryExtensions
+{
+  public static int RemoveByValue<TKey, TValue>(
+    this IDictionary<TKey, TValue> pDict,
+    Predicate<TValue> pPredicate)
+  {
+    // ISSUE: unable to decompile the method.
+  }
+
+  public static int RemoveByKey<TKey, TValue>(
+    this IDictionary<TKey, TValue> pDict,
+    Predicate<TKey> pPredicate)
+  {
+    // ISSUE: unable to decompile the method.
+  }
+}
