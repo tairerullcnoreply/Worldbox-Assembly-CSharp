23e9ffb2664b835daeb89101286081a389389053
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:26:33 -0500

    Add UserForgotPasswordWindow.cs

---
+++ UserForgotPasswordWindow.cs
@@ -0,0 +1,28 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: UserForgotPasswordWindow
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#nullable disable
+public class UserForgotPasswordWindow : MonoBehaviour
+{
+  public Button forgotPasswordButton;
+
+  public void Start()
+  {
+    if (!Config.game_loaded)
+      return;
+    ((Component) this.forgotPasswordButton).gameObject.SetActive(true);
+  }
+
+  private void OnEnable()
+  {
+    if (!Config.game_loaded)
+      return;
+    ((Component) this.forgotPasswordButton).gameObject.SetActive(true);
+  }
+}
