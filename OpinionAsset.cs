a3623cba7c737a33d0fc5ce4d4ac10fa5aac139b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:18:14 -0500

    Add OpinionAsset.cs

---
+++ OpinionAsset.cs
@@ -0,0 +1,29 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: OpinionAsset
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+[Serializable]
+public class OpinionAsset : Asset, IMultiLocalesAsset
+{
+  public string translation_key;
+  public string translation_key_negative;
+  public OpinionDelegateCalc calc;
+
+  public string getTranslationKey(int pValue)
+  {
+    return pValue > 0 || string.IsNullOrEmpty(this.translation_key_negative) ? this.translation_key : this.translation_key_negative;
+  }
+
+  public IEnumerable<string> getLocaleIDs()
+  {
+    yield return this.translation_key;
+    if (!string.IsNullOrEmpty(this.translation_key_negative))
+      yield return this.translation_key_negative;
+  }
+}
