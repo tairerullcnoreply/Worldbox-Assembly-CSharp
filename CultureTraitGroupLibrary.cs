936c595026d77325d09b592bca41d65299b962d2
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:08:34 -0500

    Add CultureTraitGroupLibrary.cs

---
+++ CultureTraitGroupLibrary.cs
@@ -0,0 +1,89 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: CultureTraitGroupLibrary
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class CultureTraitGroupLibrary : BaseCategoryLibrary<CultureTraitGroupAsset>
+{
+  public override void init()
+  {
+    base.init();
+    CultureTraitGroupAsset pAsset1 = new CultureTraitGroupAsset();
+    pAsset1.id = "harmony";
+    pAsset1.name = "trait_group_harmony";
+    pAsset1.color = "#FFE877";
+    this.add(pAsset1);
+    CultureTraitGroupAsset pAsset2 = new CultureTraitGroupAsset();
+    pAsset2.id = "architecture";
+    pAsset2.name = "trait_group_architecture";
+    pAsset2.color = "#BAF0F4";
+    this.add(pAsset2);
+    CultureTraitGroupAsset pAsset3 = new CultureTraitGroupAsset();
+    pAsset3.id = "town_plan";
+    pAsset3.name = "trait_group_town_plan";
+    pAsset3.color = "#BAF0F4";
+    this.add(pAsset3);
+    CultureTraitGroupAsset pAsset4 = new CultureTraitGroupAsset();
+    pAsset4.id = "kingdom";
+    pAsset4.name = "trait_group_kingdom";
+    pAsset4.color = "#FF6B86";
+    this.add(pAsset4);
+    CultureTraitGroupAsset pAsset5 = new CultureTraitGroupAsset();
+    pAsset5.id = "buildings";
+    pAsset5.name = "trait_group_buildings";
+    pAsset5.color = "#ADADAD";
+    this.add(pAsset5);
+    CultureTraitGroupAsset pAsset6 = new CultureTraitGroupAsset();
+    pAsset6.id = "succession";
+    pAsset6.name = "trait_group_succession";
+    pAsset6.color = "#CA59FF";
+    this.add(pAsset6);
+    CultureTraitGroupAsset pAsset7 = new CultureTraitGroupAsset();
+    pAsset7.id = "knowledge";
+    pAsset7.name = "trait_group_knowledge";
+    pAsset7.color = "#BAFFC2";
+    this.add(pAsset7);
+    CultureTraitGroupAsset pAsset8 = new CultureTraitGroupAsset();
+    pAsset8.id = "warfare";
+    pAsset8.name = "trait_group_warfare";
+    pAsset8.color = "#FF6B86";
+    this.add(pAsset8);
+    CultureTraitGroupAsset pAsset9 = new CultureTraitGroupAsset();
+    pAsset9.id = "weapons";
+    pAsset9.name = "trait_group_weapons";
+    pAsset9.color = "#FF6B86";
+    this.add(pAsset9);
+    CultureTraitGroupAsset pAsset10 = new CultureTraitGroupAsset();
+    pAsset10.id = "craft";
+    pAsset10.name = "trait_group_craft";
+    pAsset10.color = "#FF6B11";
+    this.add(pAsset10);
+    CultureTraitGroupAsset pAsset11 = new CultureTraitGroupAsset();
+    pAsset11.id = "happiness";
+    pAsset11.name = "trait_group_happiness";
+    pAsset11.color = "#FFFAA3";
+    this.add(pAsset11);
+    CultureTraitGroupAsset pAsset12 = new CultureTraitGroupAsset();
+    pAsset12.id = "worldview";
+    pAsset12.name = "trait_group_worldview";
+    pAsset12.color = "#B0FF8E";
+    this.add(pAsset12);
+    CultureTraitGroupAsset pAsset13 = new CultureTraitGroupAsset();
+    pAsset13.id = "miscellaneous";
+    pAsset13.name = "trait_group_miscellaneous";
+    pAsset13.color = "#D8D8D8";
+    this.add(pAsset13);
+    CultureTraitGroupAsset pAsset14 = new CultureTraitGroupAsset();
+    pAsset14.id = "fate";
+    pAsset14.name = "trait_group_fate";
+    pAsset14.color = "#ffd82f";
+    this.add(pAsset14);
+    CultureTraitGroupAsset pAsset15 = new CultureTraitGroupAsset();
+    pAsset15.id = "special";
+    pAsset15.name = "trait_group_special";
+    pAsset15.color = "#FF8F44";
+    this.add(pAsset15);
+  }
+}
