8537973e1fc41ee38c10a0834a9dc06f9dcacaba
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:32:21 -0500

    Add ai/behaviours/conditions/CondCurrentTileNoOtherUnits.cs

---
+++ ai/behaviours/conditions/CondCurrentTileNoOtherUnits.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ai.behaviours.conditions.CondCurrentTileNoOtherUnits
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+namespace ai.behaviours.conditions;
+
+public class CondCurrentTileNoOtherUnits : BehaviourActorCondition
+{
+  public override bool check(Actor pActor) => pActor.current_tile.countUnits() <= 1;
+}
