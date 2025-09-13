1c4a55860b75354bedf8d486685b125c08b6dc44
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:25:24 -0500

    Add TooltipSubspeciesTraitsRow.cs

---
+++ TooltipSubspeciesTraitsRow.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: TooltipSubspeciesTraitsRow
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using System.Collections.Generic;
+
+#nullable disable
+public class TooltipSubspeciesTraitsRow : TooltipTraitsRow<SubspeciesTrait>
+{
+  protected override IReadOnlyCollection<SubspeciesTrait> traits_hashset
+  {
+    get => this.tooltip_data.subspecies.getTraits();
+  }
+}
