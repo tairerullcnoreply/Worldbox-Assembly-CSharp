add2db97ab76f5d14bb93c60dcede062f16f7f7b
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:16:29 -0500

    Add MetaNeedsAndDialogueElement.cs

---
+++ MetaNeedsAndDialogueElement.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: MetaNeedsAndDialogueElement
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+#nullable disable
+public class MetaNeedsAndDialogueElement : MetaNeedsElementBase
+{
+  protected override string getText(IMetaObject pMeta, out Actor pActorResult)
+  {
+    pActorResult = (Actor) null;
+    return pMeta.countUnits() < 5 ? string.Empty : MetaTextReportHelper.getText(pMeta, pMeta.getMetaTypeAsset(), out pActorResult);
+  }
+}
