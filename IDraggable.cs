00f8f0010a5772658721395485b29573f95938b6
tairerullcnoreply <tairerullc.noreply@gmail.com>
2025-09-13 10:12:50 -0500

    Add IDraggable.cs

---
+++ IDraggable.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: IDraggable
+// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: EAF20368-35E7-4BB6-B082-5477D61019A6
+// Assembly location: C:\Users\Takko\Downloads\Worldbox .NET\Assembly-CSharp.dll
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+#nullable disable
+public interface IDraggable : IEndDragHandler, IEventSystemHandler
+{
+  Transform transform { get; }
+
+  bool spawn_particles_on_drag { get; }
+
+  bool HasComponent<T>() => ((Component) this.transform).HasComponent<T>();
+
+  void KillDrag();
+}
